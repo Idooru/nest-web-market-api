28bfd729eb5265fb8adb2449f07012c1bd644f80
Idooru <shere1765@gmail.com>
2022-05-21 11:02:34 +0900

    0.5.2.1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { HttpExceptionFilter } from "./filter/http-exception.filter";
+import { HttpExceptionFilter } from "./exceptions/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
