a67c3774252885f6956c9008811b1c39ce061f79
idooru <shere1765@gmail.com>
2023-01-17 16:50:52 +0900

    [수정] TimeLoggerLibrary파일에 로직 추가

--- src/common/lib/time.logger.library.ts
+++ src/common/lib/time.logger.library.ts
@@ -1,4 +1,21 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { Request } from "express";
 
 @Injectable()
-export class TimeLoggerLibrary {}
+export class TimeLoggerLibrary {
+  private readonly logger = new Logger("Time");
+  private now = 0;
+
+  receiveRequest(req: Request) {
+    this.logger.log(`Receive request from ${req.method} ${req.originalUrl}`);
+    this.now = Date.now();
+  }
+
+  sendResponse(req: Request) {
+    this.logger.log(
+      `Send response from ${req.method} ${req.originalUrl} :: time taken : ${
+        Date.now() - this.now
+      }ms`,
+    );
+  }
+}
