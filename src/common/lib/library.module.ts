82cca8a93768609cac38332f5d618e012c9db742
idooru <shere1765@gmail.com>
2023-01-17 16:09:36 +0900

    [추가] LibraryModule, TimeLoggerLibrary 추가

---
+++ src/common/lib/library.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { PromisesLibrary } from "./promises.library";
+import { TimeLoggerLibrary } from "./time.logger.library";
+
+@Module({
+  imports: [],
+  providers: [PromisesLibrary, TimeLoggerLibrary],
+  exports: [PromisesLibrary, TimeLoggerLibrary],
+})
+export class LibarayModule {}
