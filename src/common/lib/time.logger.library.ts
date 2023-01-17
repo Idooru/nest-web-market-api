ea4b0775d3d5dbe3ec8a44b29a7b97aafe778ee8
idooru <shere1765@gmail.com>
2023-01-17 19:52:55 +0900

    [수정] logger 프로퍼티 세분화

--- src/common/lib/time.logger.library.ts
+++ src/common/lib/time.logger.library.ts
@@ -3,16 +3,19 @@ import { Request } from "express";
 
 @Injectable()
 export class TimeLoggerLibrary {
-  private readonly logger = new Logger("Time");
+  private readonly startLogger = new Logger("TimeStart");
+  private readonly endLogger = new Logger("TimeEnd");
   private now = 0;
 
   receiveRequest(req: Request) {
-    this.logger.log(`Receive request from ${req.method} ${req.originalUrl}`);
+    this.startLogger.log(
+      `Receive request from ${req.method} ${req.originalUrl}`,
+    );
     this.now = Date.now();
   }
 
   sendResponse(req: Request) {
-    this.logger.log(
+    this.endLogger.log(
       `Send response from ${req.method} ${req.originalUrl} :: time taken : ${
         Date.now() - this.now
       }ms`,
