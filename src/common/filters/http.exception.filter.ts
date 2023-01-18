314ba4abf5b88736b10f203b07926ac636378d30
idooru <shere1765@gmail.com>
2023-01-18 21:01:11 +0900

    [수정] SRP 원칙을 준수하기 위해 AllExceptionFilter를 세분화

---
+++ src/common/filters/http.exception.filter.ts
@@ -0,0 +1,25 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from "@nestjs/common";
+import { HttpError } from "../interfaces/http.error.interface";
+import { Response } from "express";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const res = host.switchToHttp().getResponse<Response>();
+    const err = exception.getResponse() as HttpError;
+
+    return res.status(err.statusCode).setHeader("X-Powered-By", "").json({
+      success: false,
+      statusCode: err.statusCode,
+      message: "Http Exception이 발생하였습니다.",
+      timestamp: new Date().toString(),
+      reason: err.message,
+      error: err.error,
+    });
+  }
+}
