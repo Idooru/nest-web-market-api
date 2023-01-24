24e1599f86bbb872be5772028722c08030403ac2
idooru <shere1765@gmail.com>
2023-01-24 14:01:14 +0900

    [추가] 리파지토리에서 사용자를 찾기 위한 select property 추가

---
+++ src/common/config/repository-select-configs/user-select/index.ts
@@ -0,0 +1,18 @@
+export const userSelectProperty = {
+  userProfileReturnProperty: [
+    "user",
+    "Profile.realname",
+    "Profile.birth",
+    "Profile.gender",
+    "Profile.phonenumber",
+    "Auth.nickname",
+    "Auth.email",
+    "Auth.userType",
+    "Activity.bonusPoint",
+    "Activity.purchaseCount",
+    "Activity.productInquiryCount",
+    "Activity.productReviewCount",
+    "Review",
+    "Inquiry",
+  ],
+};
