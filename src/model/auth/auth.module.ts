e2687d6540a4e69f2c888b5a44f76af51524d734
idooru <shere1765@gmail.com>
2023-01-25 23:58:52 +0900

    [수정] Nestjs: secretOrPrivateKey must have a value 에러를 해결하기 위해 코드 수정

--- src/model/auth/auth.module.ts
+++ src/model/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { UserProfileEntity } from "../user/entities/user.profile.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthRepository } from "./providers/auth.repository";
 import { JwtStrategy } from "./jwt.strategy";
-import { ConfigModule } from "@nestjs/config";
 import { UserModule } from "./../user/user.module";
 import { PassportModule } from "@nestjs/passport";
 import { Module } from "@nestjs/common";
@@ -24,9 +23,14 @@ import { LibraryModule } from "src/common/lib/library.module";
       UserAuthEntity,
       UserActivityEntity,
     ]),
-    JwtModule.register({
-      secret: new ConfigService().get("JWT_SECRET"),
-      signOptions: { expiresIn: "1d" },
+    JwtModule.registerAsync({
+      // 개발 모드와 배포 모드를 따로 나눈 후 이유를 알 수 없게 .env 파일에 있는 JWT_SECRET을 읽어 올 수 없어서 에러가 났다. 해당 내용은 구글링 후 아래 url에 적힌 대로 하여 해결하였다.
+      // https://velog.io/@daep93/Nestjs-secretOrPrivateKey-must-have-a-value
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get("JWT_SECRET"),
+        signOptions: { expiresIn: "1d" },
+      }),
     }),
     forwardRef(() => UserModule),
     LibraryModule,
