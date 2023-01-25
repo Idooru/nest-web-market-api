b4959047ffd270939bc2dd2146e45320763a5a43
idooru <shere1765@gmail.com>
2023-01-25 23:57:17 +0900

    [수정] DI를 사용하기 위해 SequrityLibrary 클래스 화

--- src/common/lib/library.module.ts
+++ src/common/lib/library.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ConfigService } from "aws-sdk";
 import { PromisesLibrary } from "./promises.library";
+import { SecurityLibrary } from "./security.library";
 import { TimeLoggerLibrary } from "./time-logger.library";
 
 @Module({
-  imports: [],
-  providers: [PromisesLibrary, TimeLoggerLibrary],
-  exports: [PromisesLibrary, TimeLoggerLibrary],
+  imports: [ConfigModule],
+  providers: [
+    PromisesLibrary,
+    TimeLoggerLibrary,
+    SecurityLibrary,
+    ConfigService,
+  ],
+  exports: [PromisesLibrary, TimeLoggerLibrary, SecurityLibrary],
 })
 export class LibraryModule {}
