8bc507c7837da5b712e023e3d7e0dad6dedc3c1a
idooru <shere1765@gmail.com>
2023-01-25 22:00:10 +0900

    [수정] config 모듈화

--- src/common/config/typeorm.config.ts
+++ src/common/config/typeorm.config.ts
@@ -1,4 +1,4 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigService } from "@nestjs/config";
 import { UserEntity } from "src/model/user/entities/user.entity";
 import { UserProfileEntity } from "src/model/user/entities/user.profile.entity";
@@ -13,32 +13,38 @@ import { ReviewVideoEntity } from "src/model/upload/entities/review.video.entity
 import { InquiryEntity } from "src/model/inquiry/entities/inquiry.entity";
 import { InquiryImageEntity } from "src/model/inquiry/entities/inquiry.image.entity";
 import { InquiryVideoEntity } from "src/model/inquiry/entities/inquiry.video.entity";
+import { Module } from "@nestjs/common";
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: "mysql",
-  host: new ConfigService().get("MYSQL_HOST"),
-  port: 3306,
-  username: new ConfigService().get("MYSQL_USERNAME"),
-  password: new ConfigService().get("MYSQL_PASSWORD"),
-  database: "nestWebMarket-API",
-  entities: [
-    UserEntity,
-    UserProfileEntity,
-    UserAuthEntity,
-    UserActivityEntity,
-    ProductEntity,
-    ProductImageEntity,
-    StarRatingEntity,
-    ReviewEntity,
-    ReviewImageEntity,
-    ReviewVideoEntity,
-    InquiryEntity,
-    InquiryImageEntity,
-    InquiryVideoEntity,
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: new ConfigService().get("MYSQL_HOST"),
+      port: 3306,
+      username: new ConfigService().get("MYSQL_USERNAME"),
+      password: new ConfigService().get("MYSQL_PASSWORD"),
+      database: "nestWebMarket-API",
+      entities: [
+        UserEntity,
+        UserProfileEntity,
+        UserAuthEntity,
+        UserActivityEntity,
+        ProductEntity,
+        ProductImageEntity,
+        StarRatingEntity,
+        ReviewEntity,
+        ReviewImageEntity,
+        ReviewVideoEntity,
+        InquiryEntity,
+        InquiryImageEntity,
+        InquiryVideoEntity,
+      ],
+      migrations: [__dirname, "/src/migrations/*.ts"],
+      cli: { migrationsDir: "src/migrations" },
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true,
+    }),
   ],
-  migrations: [__dirname, "/src/migrations/*.ts"],
-  cli: { migrationsDir: "src/migrations" },
-  synchronize: false,
-  logging: true,
-  keepConnectionAlive: true,
-};
+})
+export class TypeormConfigurationModule {}
