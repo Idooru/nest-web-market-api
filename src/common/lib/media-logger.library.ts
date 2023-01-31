943f4b79235059fc2c37b4e43a4cffebd8a34f80
idooru <shere1765@gmail.com>
2023-01-31 23:12:16 +0900

    [수정] 파일이 한개인 경우와 여러개인 경우를 분리

--- src/common/lib/media-logger.library.ts
+++ src/common/lib/media-logger.library.ts
@@ -4,9 +4,23 @@ import { Injectable, Logger } from "@nestjs/common";
 export class MeidaLoggerLibrary {
   private readonly logger = new Logger("Media");
 
-  public log(mediaType: string, file: Express.Multer.File): void {
-    delete file.destination;
-    delete file.path;
-    this.logger.log(`logging ${mediaType} info ↓\n${JSON.stringify(file)}`);
+  public log(
+    mediaType: string,
+    file?: Express.Multer.File,
+    files?: Express.Multer.File[],
+  ): void {
+    if (file) {
+      delete file.destination;
+      delete file.path;
+      this.logger.log(`logging ${mediaType} info ↓\n${JSON.stringify(file)}`);
+    }
+
+    if (files) {
+      files.forEach((file) => {
+        delete file.destination;
+        delete file.path;
+      });
+      this.logger.log(`logging ${mediaType} info ↓\n${JSON.stringify(files)}`);
+    }
   }
 }
