94bfe3235cf0a22d7f03f8346a1448778adab7ef
idooru <shere1765@gmail.com>
2023-02-05 22:37:20 +0900

    [추가] 상품, 리뷰 이미지, 리뷰 비디오, 문의 이미지, 문의 비디오 select 목록 추가

--- src/common/config/repository-select-configs/media-select/index.ts
+++ src/common/config/repository-select-configs/media-select/index.ts
@@ -1,8 +1,31 @@
 export const mediaSelectProperty = {
+  productImagesSelect: [
+    "product_image.id",
+    "product_image.url",
+    "product_image.uploader",
+  ],
   reviewImagesSelect: [
-    "image.id",
-    "image.url",
-    "image.Review",
-    "image.uploader",
+    "review_image.id",
+    "review_image.url",
+    "review_image.Review",
+    "review_image.uploader",
+  ],
+  reviewVideosSelect: [
+    "review_video.id",
+    "review_video.url",
+    "review_video.Review",
+    "review_video.uploader",
+  ],
+  inquiryImagesSelect: [
+    "inquiry_image.id",
+    "inquiry_image.url",
+    "inquiry_image.Review",
+    "inquiry_image.uploader",
+  ],
+  inquiryVideosSelect: [
+    "inquiry_image.id",
+    "inquiry_image.url",
+    "inquiry_image.Review",
+    "inquiry_inage.uploader",
   ],
 };
