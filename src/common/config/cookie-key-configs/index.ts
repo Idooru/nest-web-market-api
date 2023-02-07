409e45bd629061e2e4b96b27325690080f06f0e8
idooru <shere1765@gmail.com>
2023-02-07 20:34:39 +0900

    [생성] 쿠키 키 관리를 위한 객체 생성

---
+++ src/common/config/cookie-key-configs/index.ts
@@ -0,0 +1,13 @@
+export const cookieKeys = {
+  product: {
+    image_url_cookie: "product_image_url_cookie",
+  },
+  review: {
+    image_url_cookie: "review_image_url_cookie",
+    video_url_cookie: "review_video_url_cookie",
+  },
+  inquiry: {
+    image_url_cookie: "inquiry_image_url_cookie",
+    video_url_cookie: "inquiry_video_url_cookie",
+  },
+};
