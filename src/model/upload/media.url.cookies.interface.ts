e1e9d019a4ac59058d783c7b70b320dba5625e85
idooru <shere1765@gmail.com>
2023-02-08 23:08:53 +0900

    [제거] 안쓰이는 타입 제거

--- src/model/upload/media.url.cookies.interface.ts
+++ src/model/upload/media.url.cookies.interface.ts
@@ -3,7 +3,3 @@ import { MediaReturnDto } from "./dto/media-return.dto";
 export class MediaUrlCookieValue extends MediaReturnDto {
   whatCookie: string;
 }
-
-export class MediaUrlCookies {
-  cookies: [string, MediaUrlCookieValue];
-}
