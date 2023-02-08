25c4376d412060e3c8c2c175ad1c8d29e8d13354
idooru <shere1765@gmail.com>
2023-02-08 22:29:11 +0900

    [수정] 입장토큰과 재발급토큰이 둘 중 하나라도 있으면 로그인이 안돼도록 수정

--- src/common/guards/is-not-login.guard.ts
+++ src/common/guards/is-not-login.guard.ts
@@ -8,7 +8,7 @@ import { Request } from "express";
 export class IsNotLoginGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest<Request>();
-    if (req.signedCookies.JWT_COOKIE) {
+    if (req.signedCookies.access_token || req.signedCookies.refresh_token) {
       throw new BadRequestException(
         "현재 로그인 중이므로 해당 작업을 수행할 수 없습니다.",
       );
