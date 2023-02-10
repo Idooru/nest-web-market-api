ed59457485cca8a5e53314efd3d556ede5f492a6
idooru <shere1765@gmail.com>
2023-02-10 13:27:53 +0900

    [추가] 상태 코드가 500 이상일 때 서버 로그를 확인하라는 메세지 추가

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -13,6 +13,18 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const res = host.switchToHttp().getResponse<Response>();
     const err = exception.getResponse() as HttpExceptionType;
 
+    if (err.statusCode >= 500) {
+      const warn = err.message.concat(" 서버 로그를 확인하세요.");
+      return res.status(err.statusCode).setHeader("X-Powered-By", "").json({
+        success: false,
+        statusCode: err.statusCode,
+        message: "Http Exception이 발생하였습니다.",
+        timestamp: new Date().toString(),
+        reason: warn,
+        error: err.error,
+      });
+    }
+
     return res.status(err.statusCode).setHeader("X-Powered-By", "").json({
       success: false,
       statusCode: err.statusCode,
