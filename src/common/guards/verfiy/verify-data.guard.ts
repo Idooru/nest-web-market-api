03fbf872fc6df18056eebb4bb05dc1afaf073415
idooru <shere1765@gmail.com>
2023-02-18 22:07:34 +0900

    [수정] 받아온 쿠키에서 필터링 하는 로직과 필요한 쿠키와 받아온 쿠키 비교 로직 수정

--- src/common/guards/verfiy/verify-data.guard.ts
+++ src/common/guards/verfiy/verify-data.guard.ts
@@ -18,15 +18,21 @@ export class VerifyDataGuard implements CanActivate {
     const req = context.switchToHttp().getRequest<Request>();
     const res = context.switchToHttp().getResponse<Response>();
 
-    const importedCookies = Object.keys(req.signedCookies).filter((cookie) =>
-      cookie.includes("is"),
-    );
     const needCookies = this.data;
+    const importedCookies = Object.keys(req.signedCookies).filter((cookie) =>
+      needCookies.includes(cookie),
+    );
 
-    if (
-      JSON.stringify(importedCookies.sort()) !==
-      JSON.stringify(needCookies.sort())
-    ) {
+    const sortedNeedCookies = needCookies.sort();
+    const sortedImportedCookies = needCookies.sort();
+
+    const sameArray =
+      needCookies.length === importedCookies.length &&
+      sortedNeedCookies.every(
+        (cookieKey, idx) => cookieKey === sortedImportedCookies[idx],
+      );
+
+    if (!sameArray) {
       throw new UnauthorizedException(
         "검증에 필요한 쿠키가 일치하지 않았기 때문에 해당 로직을 사용할 수 없습니다. 검증 API를 먼저 실행시켜 주세요.",
       );
