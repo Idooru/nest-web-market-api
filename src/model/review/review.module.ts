cc01a219fc587fcf9fd9e5bdad8854f1edc82dec
idooru <shere1765@gmail.com>
2023-02-20 15:19:50 +0900

    [수정] 의존성 경로 수정

--- src/model/review/review.module.ts
+++ src/model/review/review.module.ts
@@ -1,14 +1,14 @@
 import { ProductModule } from "../product/product.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module, forwardRef } from "@nestjs/common";
-import { ReviewService } from "./providers/review.service";
+import { ReviewService } from "./services/review.service";
 import { ReviewVersionOneFreeUseController } from "./controllers/review-v1-free-use.controller";
 import { ReviewEntity } from "./entities/review.entity";
 import { UserModule } from "../user/user.module";
-import { ReviewRepository } from "./providers/review.repository";
+import { ReviewRepository } from "./repositories/review.repository";
 import { StarRatingEntity } from "./entities/star-rating.entity";
-import { StarRatingRepository } from "./providers/star-rating.repository";
-import { StarRatingService } from "./providers/star-rating.service";
+import { StarRatingRepository } from "./repositories/star-rating.repository";
+import { StarRatingService } from "./services/star-rating.service";
 import { UploadModule } from "../upload/upload.module";
 import { LibraryModule } from "src/common/lib/library.module";
 import { JwtModule } from "@nestjs/jwt";
