44f3e5c5bcf671ceb0600e49a16babd797843e64
idooru <shere1765@gmail.com>
2023-02-20 15:19:09 +0900

    [생성] 기존에 providers 폴더에 있던 파일을 repositories 폴더로 분리

--- src/model/review/repositories/review.repository.ts
+++ src/model/review/repositories/review.repository.ts
@@ -4,7 +4,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { UserActivityEntity } from "src/model/user/entities/user.activity.entity";
 import { ModifyReviewDao } from "../dto/modify-review.dto";
-import { CreateReviewDao } from "./../dto/create-review.dto";
+import { CreateReviewDao } from "../dto/create-review.dto";
 
 @Injectable()
 export class ReviewRepository {
