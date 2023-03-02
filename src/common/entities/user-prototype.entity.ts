1956e99326f6db953b7d1da55798577e28ff4381
idooru <shere1765@gmail.com>
2023-03-02 12:22:18 +0900

    [수정] 필드 이름 수정

--- src/common/entities/user-prototype.entity.ts
+++ src/common/entities/user-prototype.entity.ts
@@ -6,7 +6,7 @@ import { CommonEntity } from "./common.entity";
 @Entity({ name: "users_prototype", synchronize: true })
 export class UserPrototypeEntity extends CommonEntity {
   @Column({ type: "enum", enum: ["client", "admin"] })
-  userType: ["client", "admin"];
+  type: ["client", "admin"];
 
   @OneToOne(() => UserProfileEntity, (profile) => profile.UserPrototype, {
     cascade: true,
