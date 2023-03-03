f69068b712d41787c728081e882aa8f66b3e986c
idooru <shere1765@gmail.com>
2023-03-03 20:38:27 +0900

    [수정] 개발 모드가 아닐 때만 VerifyDataGuard 로직이 수행되도록 수정

--- src/common/guards/verfiy/verify-data.guard.ts
+++ src/common/guards/verfiy/verify-data.guard.ts
@@ -15,31 +15,34 @@ export class VerifyDataGuard implements CanActivate {
   }
 
   canActivate(context: ExecutionContext): boolean {
-    const req = context.switchToHttp().getRequest<Request>();
-    const res = context.switchToHttp().getResponse<Response>();
+    if (process.env.NODE_ENV !== "dev") {
+      const req = context.switchToHttp().getRequest<Request>();
+      const res = context.switchToHttp().getResponse<Response>();
 
-    const needCookies = this.data;
-    const importedCookies = Object.keys(req.signedCookies).filter((cookie) =>
-      needCookies.includes(cookie),
-    );
-
-    const sortedNeedCookies = needCookies.sort();
-    const sortedImportedCookies = importedCookies.sort();
-
-    const isEqual =
-      needCookies.length === importedCookies.length &&
-      sortedNeedCookies.every(
-        (cookieKey, idx) => cookieKey === sortedImportedCookies[idx],
+      const needCookies = this.data;
+      const importedCookies = Object.keys(req.signedCookies).filter((cookie) =>
+        needCookies.includes(cookie),
       );
 
-    if (!isEqual) {
-      throw new UnauthorizedException(
-        "검증에 필요한 쿠키가 일치하지 않았기 때문에 해당 로직을 사용할 수 없습니다. 검증 API를 먼저 실행시켜 주세요.",
-      );
+      const sortedNeedCookies = needCookies.sort();
+      const sortedImportedCookies = importedCookies.sort();
+
+      const isEqual =
+        needCookies.length === importedCookies.length &&
+        sortedNeedCookies.every(
+          (cookieKey, idx) => cookieKey === sortedImportedCookies[idx],
+        );
+
+      if (!isEqual) {
+        throw new UnauthorizedException(
+          "검증에 필요한 쿠키가 일치하지 않았기 때문에 해당 로직을 사용할 수 없습니다. 검증 API를 먼저 실행시켜 주세요.",
+        );
+      }
+
+      importedCookies.forEach((cookie) => res.clearCookie(cookie));
+
+      return true;
     }
-
-    importedCookies.forEach((cookie) => res.clearCookie(cookie));
-
     return true;
   }
 }
