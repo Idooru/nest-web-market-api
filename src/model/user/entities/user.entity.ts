bf82511d7dc3901a5e3b93d7a1141d3a6a9360e4
idooru <shere1765@gmail.com>
2023-03-03 15:05:54 +0900

    [수정] user-prototype 대신 user로 이름 사용

---
+++ src/model/user/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { IsNotEmpty } from "class-validator";
+import { AdminUserEntity } from "src/model/user/entities/admin-user.entity";
+import { ClientUserEntity } from "src/model/user/entities/client-user.entity";
+import { Column, Entity, OneToOne } from "typeorm";
+import { UserAuthEntity } from "./user.auth.entity";
+import { UserProfileEntity } from "./user.profile.entity";
+import { CommonEntity } from "../../../common/entities/common.entity";
+
+@Entity({ name: "users", synchronize: true })
+export class UserEntity extends CommonEntity {
+  @IsNotEmpty()
+  @Column({ type: "enum", enum: ["client", "admin"] })
+  type: ["client", "admin"];
+
+  @OneToOne(() => UserProfileEntity, (profile) => profile.User, {
+    cascade: true,
+  })
+  Profile: UserProfileEntity;
+
+  @OneToOne(() => UserAuthEntity, (auth) => auth.User, {
+    cascade: true,
+  })
+  Auth: UserAuthEntity;
+
+  @OneToOne(() => ClientUserEntity, (client) => client.User, {
+    cascade: true,
+  })
+  Client: ClientUserEntity;
+
+  @OneToOne(() => AdminUserEntity, (admin) => admin.User, {
+    cascade: true,
+  })
+  Admin: AdminUserEntity;
+}
