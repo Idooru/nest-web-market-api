962571d8caf82a31057ffd988531957bc1dc330e
idooru <shere1765@gmail.com>
2023-03-03 15:05:14 +0900

    [생성] 사용자를 생성할 때 필요한 기본 정보로 사용되는 DTO 생성

--- src/model/user/dtos/create-user-base.dto.ts
+++ src/model/user/dtos/create-user-base.dto.ts
@@ -1,9 +1,8 @@
-import { UserActivityEntity } from "../entities/user.activity.entity";
 import { UserAuthEntity } from "../entities/user.auth.entity";
 import { UserProfileEntity } from "../entities/user.profile.entity";
 
-export class CreateUserDto {
+export class CreateUserBaseDto {
   Profile: UserProfileEntity;
   Auth: UserAuthEntity;
-  Activity: UserActivityEntity;
+  type: ["client", "admin"];
 }
