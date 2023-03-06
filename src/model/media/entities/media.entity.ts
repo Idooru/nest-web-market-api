f3ae1c9d15e8e2349db9efbced3d066272511e4a
idooru <shere1765@gmail.com>
2023-03-06 23:34:12 +0900

    [추가] 문자열인지 확인하는 validator 추가

--- src/model/media/entities/media.entity.ts
+++ src/model/media/entities/media.entity.ts
@@ -1,13 +1,15 @@
 import { CommonEntity } from "src/common/entities/common.entity";
-import { IsEmail, IsUrl } from "class-validator";
+import { IsEmail, IsString, IsUrl } from "class-validator";
 import { Column } from "typeorm";
 
 export class MediaEntity extends CommonEntity {
   @IsUrl()
+  @IsString()
   @Column({ type: "varchar", nullable: false, unique: true })
   url: string;
 
   @IsEmail()
+  @IsString()
   @Column({ type: "varchar", nullable: false })
   uploader: string;
 }
