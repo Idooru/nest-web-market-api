7d4eeacd3982c7e247532ae7ea37450229e4abc3
idooru <shere1765@gmail.com>
2023-03-06 14:27:21 +0900

    [수정] 메서드 이름 변경

--- src/model/user/controllers/v1/user-v1-only-admin.controller.ts
+++ src/model/user/controllers/v1/user-v1-only-admin.controller.ts
@@ -12,7 +12,6 @@ import { IsLoginGuard } from "src/common/guards/authenticate/is-login.guard";
 import { VerifyDataGuard } from "src/common/guards/verfiy/verify-data.guard";
 import { JsonGeneralInterface } from "src/common/interceptors/general/interface/json-general-interface";
 import { JsonGeneralInterceptor } from "src/common/interceptors/general/json-general.interceptor";
-
 import { UserEntity } from "../../entities/user.entity";
 import { UserGeneralService } from "../../services/user-general.service";
 
@@ -24,33 +23,33 @@ export class UserVersionOneOnlyAdminController {
 
   @UseInterceptors(JsonGeneralInterceptor)
   @Get("/")
-  async getUsersAllFromLastest(): Promise<JsonGeneralInterface<UserEntity[]>> {
+  async findAllUsersFromLatest(): Promise<JsonGeneralInterface<UserEntity[]>> {
     return {
       statusCode: 200,
       message: "전체 사용자 정보를 최신 순서로 가져옵니다.",
-      result: await this.userGeneralService.getUsersAllFromLastest(),
+      result: await this.userGeneralService.findAllUsersFromLatest(),
     };
   }
 
   @UseInterceptors(JsonGeneralInterceptor)
   @Get("/oldest")
-  async getUsersAllFromOldest(): Promise<JsonGeneralInterface<UserEntity[]>> {
+  async findAllUsersFromOldest(): Promise<JsonGeneralInterface<UserEntity[]>> {
     return {
       statusCode: 200,
       message: "전체 사용자 정보를 오래된 순서로 가져옵니다.",
-      result: await this.userGeneralService.getUsersAllFromOldest(),
+      result: await this.userGeneralService.findAllUsersFromOldest(),
     };
   }
 
   @UseInterceptors(JsonGeneralInterceptor)
   @Get("/:id")
-  async getUserById(
+  async findClientUserInfoFromAdmin(
     @Param("id") userId: string,
   ): Promise<JsonGeneralInterface<UserEntity>> {
     return {
       statusCode: 200,
       message: `${userId}에 해당하는 사용자 정보를 가져옵니다.`,
-      result: await this.userGeneralService.findUserInfoFromAdmin(userId),
+      result: await this.userGeneralService.findClientUserInfoFromAdmin(userId),
     };
   }
 
@@ -60,7 +59,7 @@ export class UserVersionOneOnlyAdminController {
   async kickUser(
     @Param("id") userId: string,
   ): Promise<JsonGeneralInterface<void>> {
-    await this.userGeneralService.deleteUserWithId(userId);
+    await this.userGeneralService.deleteUser(userId);
 
     return {
       statusCode: 200,
