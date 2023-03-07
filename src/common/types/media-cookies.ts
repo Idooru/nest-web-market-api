f7c9e445041eae6cafc3eba301c6f7c317dad77e
idooru <shere1765@gmail.com>
2023-03-07 13:42:17 +0900

    [생성] 미디어 관련 쿠키 타입 생성

---
+++ src/common/types/media-cookies.ts
@@ -0,0 +1,6 @@
+export type MediaCookies = [
+  string,
+  { whatCookie: string; url: string; fileName: string },
+];
+
+export type MediaCookie = { whatCookie: string; url: string; fileName: string };
