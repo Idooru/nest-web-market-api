95cd80d2e9aaea6b098d2c9abf92bc51adb88d4c
idooru <shere1765@gmail.com>
2023-03-07 15:14:42 +0900

    [수정] 필터링 로직을 cookiesFilter함수로 수정

--- src/common/decorators/cookies.decorator.ts
+++ src/common/decorators/cookies.decorator.ts
@@ -1,12 +1,12 @@
 import { createParamDecorator, ArgumentsHost } from "@nestjs/common";
 import { Request } from "express";
-import { filtering } from "./filtering";
+import { cookiesFilter } from "./cookies-filter";
 
 export const Cookies = createParamDecorator(
   (data: string, context: ArgumentsHost): Array<unknown> => {
     const req = context.switchToHttp().getRequest() as Request;
 
-    filtering(req, data);
+    cookiesFilter(req, data);
 
     return Object.entries(req.signedCookies)
       .filter((cookie) => cookie[0].includes(data))
