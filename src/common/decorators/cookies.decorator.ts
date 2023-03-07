c555edf52d9e0e0b67e7dc000c03bd03b4da3f9f
idooru <shere1765@gmail.com>
2023-03-07 14:57:08 +0900

    [추가] filtering 함수 추가

--- src/common/decorators/cookies.decorator.ts
+++ src/common/decorators/cookies.decorator.ts
@@ -1,20 +1,12 @@
 import { createParamDecorator, ArgumentsHost } from "@nestjs/common";
-import { BadRequestException } from "@nestjs/common/exceptions";
 import { Request } from "express";
+import { filtering } from "./filtering";
 
 export const Cookies = createParamDecorator(
   (data: string, context: ArgumentsHost): Array<unknown> => {
     const req = context.switchToHttp().getRequest() as Request;
 
-    const filtering = Object.keys(req.signedCookies).filter((cookie) =>
-      cookie.includes(data),
-    );
-
-    if (!filtering.length) {
-      throw new BadRequestException(
-        `데이터(${data})에 해당하는 쿠키를 가져올 수 없습니다.`,
-      );
-    }
+    filtering(req, data);
 
     return Object.entries(req.signedCookies)
       .filter((cookie) => cookie[0].includes(data))
