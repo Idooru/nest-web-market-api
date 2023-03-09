dcb04fb60f43e335c93e7d4f42a3da24977244c4
idooru <shere1765@gmail.com>
2023-03-09 20:07:59 +0900

    [수정] 식별자 다소 변경

--- src/model/review/entities/review.entity.ts
+++ src/model/review/entities/review.entity.ts
@@ -1,33 +1,29 @@
-import { Column, Entity, OneToMany, ManyToOne, JoinColumn } from "typeorm";
+import { Column, Entity, OneToMany, ManyToOne } from "typeorm";
 import { ProductEntity } from "./../../product/entities/product.entity";
 import { CommonEntity } from "src/common/entities/common.entity";
 import { IsNotEmpty, IsString, IsEnum } from "class-validator";
 import { ReviewImageEntity } from "src/model/media/entities/review.image.entity";
 import { ReviewVideoEntity } from "src/model/media/entities/review.video.entity";
-import { UserActivityEntity } from "../../user/entities/user.activity.entity";
+import { ClientUserEntity } from "src/model/user/entities/client-user.entity";
 
 @Entity({ name: "reviews", synchronize: true })
 export class ReviewEntity extends CommonEntity {
   @IsEnum([1, 2, 3, 4, 5])
   @IsNotEmpty()
   @Column({ type: "enum", enum: [1, 2, 3, 4, 5] })
-  userSelectScore: 1 | 2 | 3 | 4 | 5;
+  scoreChosenByClient: 1 | 2 | 3 | 4 | 5;
 
   @IsString()
   @IsNotEmpty()
   @Column({ type: "text", nullable: false })
   reviews: string;
 
-  @ManyToOne(() => UserActivityEntity, (activity) => activity.Review, {
-    onDelete: "CASCADE",
-  })
-  @JoinColumn({ name: "userActivityId" })
-  UserActivity: UserActivityEntity;
+  @ManyToOne(() => ClientUserEntity, (clientUser) => clientUser.writtenReview)
+  reviewer: ClientUserEntity;
 
   @ManyToOne(() => ProductEntity, (product) => product.Review, {
     onDelete: "CASCADE",
   })
-  @JoinColumn({ name: "productId" })
   Product: ProductEntity;
 
   @OneToMany(() => ReviewImageEntity, (image) => image.Review, {
