4a2549bc1eef7ef48b3370723e1c638b0e3f9372
idooru <shere1765@gmail.com>
2023-03-12 20:14:18 +0900

    [추가] 에러 로깅 기능 추가

--- src/model/review/repositories/star-rate-general.repository.ts
+++ src/model/review/repositories/star-rate-general.repository.ts
@@ -2,111 +2,132 @@ import { Repository } from "typeorm";
 import { StarRateEntity } from "../entities/star-rate.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ProductEntity } from "src/model/product/entities/product.entity";
-import { ReviewEntity } from "../entities/review.entity";
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from "@nestjs/common";
 
+@Injectable()
 export class StarRateRepository {
   constructor(
     @InjectRepository(StarRateEntity)
     private readonly starRateRepository: Repository<StarRateEntity>,
   ) {}
 
-  async createStarRateSample(): Promise<StarRateEntity> {
-    const StarRate = this.starRateRepository.create({});
-    return await this.starRateRepository.save(StarRate);
-  }
+  private readonly logger = new Logger("Repositroy");
 
-  async starRatingAndSum(
-    StarRate: StarRateEntity,
-    scoreChosenByClient: number,
-  ): Promise<void> {
-    switch (scoreChosenByClient) {
-      case 1:
-        ++StarRate.onePointCount;
-        StarRate.onePointSum += scoreChosenByClient;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 2:
-        ++StarRate.twoPointCount;
-        StarRate.twoPointSum += scoreChosenByClient;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 3:
-        ++StarRate.threePointCount;
-        StarRate.threePointSum += scoreChosenByClient;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 4:
-        ++StarRate.fourPointCount;
-        StarRate.fourPointSum += scoreChosenByClient;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 5:
-        ++StarRate.fivePointCount;
-        StarRate.fivePointSum += scoreChosenByClient;
-        await this.starRateRepository.save(StarRate);
-        break;
+  async createStarRateSample(): Promise<StarRateEntity> {
+    try {
+      const StarRate = this.starRateRepository.create({});
+      return await this.starRateRepository.save(StarRate);
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
     }
   }
 
-  async decreaseStarRateAndMinus(
+  async increaseStarRate(
+    StarRate: StarRateEntity,
+    scoreChosenByClient: number,
+  ): Promise<void> {
+    try {
+      switch (scoreChosenByClient) {
+        case 1:
+          ++StarRate.onePointCount;
+          StarRate.onePointSum += scoreChosenByClient;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 2:
+          ++StarRate.twoPointCount;
+          StarRate.twoPointSum += scoreChosenByClient;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 3:
+          ++StarRate.threePointCount;
+          StarRate.threePointSum += scoreChosenByClient;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 4:
+          ++StarRate.fourPointCount;
+          StarRate.fourPointSum += scoreChosenByClient;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 5:
+          ++StarRate.fivePointCount;
+          StarRate.fivePointSum += scoreChosenByClient;
+          await this.starRateRepository.save(StarRate);
+          break;
+      }
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async decreaseStarRate(
     StarRate: StarRateEntity,
     beforeScore: number,
   ): Promise<void> {
-    switch (beforeScore) {
-      case 1:
-        --StarRate.onePointCount;
-        StarRate.onePointSum -= beforeScore;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 2:
-        --StarRate.twoPointCount;
-        StarRate.twoPointSum -= beforeScore;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 3:
-        --StarRate.threePointCount;
-        StarRate.threePointSum -= beforeScore;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 4:
-        --StarRate.fourPointCount;
-        StarRate.fourPointSum -= beforeScore;
-        await this.starRateRepository.save(StarRate);
-        break;
-      case 5:
-        --StarRate.fivePointCount;
-        StarRate.fivePointSum -= beforeScore;
-        await this.starRateRepository.save(StarRate);
-        1;
-        break;
+    try {
+      switch (beforeScore) {
+        case 1:
+          --StarRate.onePointCount;
+          StarRate.onePointSum -= beforeScore;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 2:
+          --StarRate.twoPointCount;
+          StarRate.twoPointSum -= beforeScore;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 3:
+          --StarRate.threePointCount;
+          StarRate.threePointSum -= beforeScore;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 4:
+          --StarRate.fourPointCount;
+          StarRate.fourPointSum -= beforeScore;
+          await this.starRateRepository.save(StarRate);
+          break;
+        case 5:
+          --StarRate.fivePointCount;
+          StarRate.fivePointSum -= beforeScore;
+          await this.starRateRepository.save(StarRate);
+          1;
+          break;
+      }
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
     }
   }
 
-  async modifyStarRateng(
-    StarRate: StarRateEntity,
-    scoreChosenByClient: number,
-    review: ReviewEntity,
-  ) {
-    const beforeScore = review.scoreChosenByClient;
-    if (beforeScore === scoreChosenByClient) return;
-
-    await this.decreaseStarRateAndMinus(StarRate, beforeScore);
-    await this.starRatingAndSum(StarRate, scoreChosenByClient);
-  }
-
   async findStarRateWithId(StarRateId: string): Promise<StarRateEntity> {
-    return await this.starRateRepository
-      .createQueryBuilder("StarRate")
-      .where("StarRate.id = :id", { id: StarRateId })
-      .getOne();
+    try {
+      return await this.starRateRepository
+        .createQueryBuilder()
+        .select(["starRate"])
+        .from(StarRateEntity, "starRate")
+        .where("starRate.id = :id", { id: StarRateId })
+        .getOne();
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 
   async insertTotalScoreOnStarRate(averageScore: number, starRateId: string) {
-    const StarRate = await this.findStarRateWithId(starRateId);
+    try {
+      const StarRate = await this.findStarRateWithId(starRateId);
 
-    StarRate.averageScore = averageScore;
-    await this.starRateRepository.save(StarRate);
-    // await this.starRateRepository.update(id, { averageScore });
+      StarRate.averageScore = averageScore;
+      await this.starRateRepository.save(StarRate);
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 
   async insertProductIdOnStarRate(
