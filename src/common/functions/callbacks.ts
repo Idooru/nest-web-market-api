be4898b34bec556d8dad2be16782dcca06a74916
idooru <shere1765@gmail.com>
2023-03-17 20:15:11 +0900

    [생성] 중첩되는 콜백함수를 사용하기 위한 callbacks 생성

---
+++ src/common/functions/callbacks.ts
@@ -0,0 +1,7 @@
+import { MediaEntity } from "src/model/media/entities/media.entity";
+
+export const brieflyFileName = (media: MediaEntity) =>
+  media.url.replace(
+    `${process.env.APPLICATION_SCHEME}://${process.env.APPLICATION_HOST}:${process.env.APPLICATION_PORT}/media/`,
+    "",
+  );
