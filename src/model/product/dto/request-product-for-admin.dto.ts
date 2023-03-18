cac33c49aad96c2536da6a77ffcbaa6a3b426ff8
idooru <shere1765@gmail.com>
2023-03-18 18:53:18 +0900

    [수정] 파일 이름 수정

--- src/model/product/dto/request-product-for-admin.dto.ts
+++ src/model/product/dto/request-product-for-admin.dto.ts
@@ -1,7 +1,7 @@
 import { PickType } from "@nestjs/swagger";
 import { ProductEntity } from "../entities/product.entity";
 
-export class ReturnProductsDto extends PickType(ProductEntity, [
+export class RequestProductForAdminDto extends PickType(ProductEntity, [
   "id",
   "name",
   "type",
