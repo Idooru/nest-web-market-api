52bd0f3bd40111e432dffd066f74ff37e1bb6028
idooru <shere1765@gmail.com>
2023-03-18 21:58:17 +0900

    [생성] accessory 서비스를 묶어서 편리하게 사용하기 위한 bundle 서비스 생성

---
+++ src/model/media/services/media-bundle.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { RequestMediaDto } from "../dto/request-media.dto";
+import { MediaAccessoryService } from "./media-accessory.service";
+
+@Injectable()
+export class MediaBundleService {
+  constructor(private readonly mediaAccessoryService: MediaAccessoryService) {}
+
+  async deleteMediaFile(mediaCookies: RequestMediaDto[], mediaPath: string) {
+    if (mediaCookies.length >= 2) {
+      for (const cookie of mediaCookies) {
+        this.mediaAccessoryService.deleteMediaFilesOnServerDisk(
+          cookie.fileName,
+          mediaPath,
+        );
+      }
+    } else {
+      this.mediaAccessoryService.deleteMediaFilesOnServerDisk(
+        mediaCookies[0].fileName,
+        mediaPath,
+      );
+    }
+  }
+}
---
+++ src/model/review/services/review-bundle.service.ts
@@ -0,0 +1,133 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { UserGeneralRepository } from "src/model/user/repositories/user-general.repository";
+import { InsertMediaDto } from "../dto/insert-media.dto";
+import { ModifyMediaDto } from "../dto/modify-media.dto";
+import { PushMediaDto } from "../dto/push-media.dto";
+import { ReviewEntity } from "../entities/review.entity";
+import { ReviewGeneralRepository } from "../repositories/review-general.repository";
+import { ReviewAccessoryService } from "./review-accessory.service";
+import { MediaAccessoryService } from "src/model/media/services/media-accessory.service";
+import { brieflyFileName } from "src/common/functions/callbacks";
+
+@Injectable()
+export class ReviewBundleService {
+  constructor(
+    private readonly userGeneralRepository: UserGeneralRepository,
+    private readonly reviewGeneralRepository: ReviewGeneralRepository,
+    private readonly reviewAccessoryService: ReviewAccessoryService,
+    private readonly mediaAccessoryService: MediaAccessoryService,
+  ) {}
+
+  async distinguishOwnReview(
+    reviewId: string,
+    userId: string,
+  ): Promise<ReviewEntity> {
+    const client = await this.userGeneralRepository.findClientUserObject(
+      userId,
+    );
+
+    const reviews = await this.reviewGeneralRepository.findAllClientsReviews(
+      client.id,
+    );
+
+    const review = reviews.find((idx) => idx.id === reviewId);
+
+    if (!review) {
+      // 만약 리뷰를 하나도 작성하지 않은 사용자가 다른 사용자의 리뷰를 수정하려고 시도할시 아래 예외가 발생한다.
+      throw new NotFoundException(
+        `고객 사용자의 아이디(${client.id})로 작성된 리뷰중에 reviewId(${reviewId})와 같은 리뷰를 찾을 수 없습니다.`,
+      );
+    }
+
+    return review;
+  }
+
+  async pushMedia(pushMediaDto: PushMediaDto) {
+    const { reviewBody, reviewImgCookies, reviewVdoCookies } = pushMediaDto;
+
+    if (reviewImgCookies) {
+      await this.reviewAccessoryService.pushReviewImageInDto({
+        reviewBody,
+        reviewImgCookies,
+      });
+    }
+    if (reviewVdoCookies) {
+      await this.reviewAccessoryService.pushReviewVideoInDto({
+        reviewBody,
+        reviewVdoCookies,
+      });
+    }
+  }
+
+  async insertMedia(insertMediaDto: InsertMediaDto) {
+    const { reviewImgCookies, reviewVdoCookies, reviewBody, review } =
+      insertMediaDto;
+
+    if (reviewImgCookies) {
+      await this.reviewAccessoryService.distinguishReviewImagesCountForInsert(
+        reviewImgCookies,
+        reviewBody,
+        review,
+      );
+    }
+
+    if (reviewVdoCookies) {
+      await this.reviewAccessoryService.distinguishReviewVideosCountForInsert(
+        reviewVdoCookies,
+        reviewBody,
+        review,
+      );
+    }
+  }
+
+  async modifyMedia(modifyMediaDto: ModifyMediaDto) {
+    const { reviewBody, review, reviewImgCookies, reviewVdoCookies } =
+      modifyMediaDto;
+
+    if (reviewImgCookies) {
+      this.reviewAccessoryService.distinguishReviewImagesCountForModify(
+        reviewImgCookies,
+        reviewBody,
+        review,
+      );
+    }
+
+    if (reviewVdoCookies) {
+      this.reviewAccessoryService.distinguishReviewVideosCountForModify(
+        reviewVdoCookies,
+        reviewBody,
+        review,
+      );
+    }
+  }
+
+  async deleteMediaIfItHad(review: ReviewEntity) {
+    if (review.Image.length >= 1) {
+      review.Image.map(brieflyFileName).forEach((name) =>
+        this.mediaAccessoryService.deleteMediaFilesOnServerDisk(
+          name,
+          "/image/review",
+        ),
+      );
+
+      await this.reviewAccessoryService.distinguishReviewImagesCountForDelete(
+        review.Image,
+        review,
+      );
+    }
+
+    if (review.Video.length >= 1) {
+      review.Video.map(brieflyFileName).forEach((name) =>
+        this.mediaAccessoryService.deleteMediaFilesOnServerDisk(
+          name,
+          "/video/review",
+        ),
+      );
+
+      await this.reviewAccessoryService.distinguishReviewVideosCountForDelete(
+        review.Video,
+        review,
+      );
+    }
+  }
+}
