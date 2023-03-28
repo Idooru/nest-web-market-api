a9d594bda177063215493d006265a8ad05c303e9
idooru <shere1765@gmail.com>
2023-03-28 14:03:18 +0900

    [추가] 문의 응답 엔티티와 1:1 관계를 맺기 위한 컬럼 추가

--- src/model/inquiry/entities/inquiry-request.entity.ts
+++ src/model/inquiry/entities/inquiry-request.entity.ts
@@ -1,10 +1,11 @@
 import { IsEnum, IsNotEmpty } from "class-validator";
 import { ProductEntity } from "src/model/product/entities/product.entity";
-import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany, OneToOne } from "typeorm";
 import { InquiryRequestImageEntity } from "src/model/media/entities/inquiry-request-image.entity";
 import { InquiryRequestVideoEntity } from "src/model/media/entities/inquiry-request-video.entity";
 import { ClientUserEntity } from "src/model/user/entities/client-user.entity";
 import { PostEntity } from "src/common/entities/post.entity";
+import { InquiryResponseEntity } from "./inquiry-response.entity";
 
 @Entity({ name: "inquiry_requests", synchronize: true })
 export class InquiryRequestEntity extends PostEntity {
@@ -21,13 +22,20 @@ export class InquiryRequestEntity extends PostEntity {
   })
   Product: ProductEntity;
 
-  @OneToMany(() => InquiryRequestImageEntity, (image) => image.Inquiry, {
+  @OneToMany(() => InquiryRequestImageEntity, (image) => image.InquiryRequest, {
     cascade: true,
   })
   Image: InquiryRequestImageEntity[];
 
-  @OneToMany(() => InquiryRequestVideoEntity, (video) => video.Inquiry, {
+  @OneToMany(() => InquiryRequestVideoEntity, (video) => video.InquiryRequest, {
     cascade: true,
   })
   Video: InquiryRequestVideoEntity[];
+
+  @OneToOne(
+    () => InquiryResponseEntity,
+    (inquiryResponse) => inquiryResponse.InquiryRequest,
+    { cascade: true },
+  )
+  InquiryResponse: InquiryResponseEntity;
 }
