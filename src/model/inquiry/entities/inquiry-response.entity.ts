fcae2ce81cc52a3f0b703012fcd53ce532c94a12
idooru <shere1765@gmail.com>
2023-03-28 14:05:30 +0900

    [생성] 문의 응답 엔티티 생성

---
+++ src/model/inquiry/entities/inquiry-response.entity.ts
@@ -0,0 +1,47 @@
+import { IsEnum, IsNotEmpty } from "class-validator";
+import { PostEntity } from "src/common/entities/post.entity";
+import { InquiryResponseImageEntity } from "src/model/media/entities/inquiry-response-image.entity";
+import { InquiryResponseVideoEntity } from "src/model/media/entities/inquiry-response-video.entity";
+import { ClientUserEntity } from "src/model/user/entities/client-user.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+} from "typeorm";
+import { InquiryRequestEntity } from "./inquiry-request.entity";
+
+@Entity({ name: "inquiry_responses", synchronize: true })
+export class InquiryResponseEntity extends PostEntity {
+  @IsEnum(["product status", "delivery status", ""])
+  @IsNotEmpty()
+  @Column({ type: "enum", enum: ["product status", "delivery status"] })
+  categories: "product status" | "delivery status";
+
+  @ManyToOne(() => ClientUserEntity, (client) => client.receiveInquiry)
+  receiver: ClientUserEntity;
+
+  @OneToMany(
+    () => InquiryResponseImageEntity,
+    (image) => image.InquiryResponse,
+    { cascade: true },
+  )
+  Image: InquiryResponseImageEntity[];
+
+  @OneToMany(
+    () => InquiryResponseVideoEntity,
+    (video) => video.InquiryResponse,
+    { cascade: true },
+  )
+  Video: InquiryResponseVideoEntity[];
+
+  @OneToOne(
+    () => InquiryRequestEntity,
+    (inquiryRequest) => inquiryRequest.InquiryResponse,
+    { onDelete: "CASCADE" },
+  )
+  @JoinColumn({ name: "inquiryRequestId" })
+  InquiryRequest: InquiryRequestEntity;
+}
