516ec4a12f3723c5478d94b8e648f03bc47279a7
idooru <shere1765@gmail.com>
2023-03-28 14:12:43 +0900

    [수정] 필드 앞글자를 대문자로 수정

--- src/model/media/entities/inquiry-request-image.entity.ts
+++ src/model/media/entities/inquiry-request-image.entity.ts
@@ -4,8 +4,10 @@ import { MediaEntity } from "../../../common/entities/media.entity";
 
 @Entity({ name: "inquiry_request_images", synchronize: true })
 export class InquiryRequestImageEntity extends MediaEntity {
-  @ManyToOne(() => InquiryRequestEntity, (inquiry) => inquiry.Image, {
-    onDelete: "CASCADE",
-  })
-  Inquiry: InquiryRequestEntity;
+  @ManyToOne(
+    () => InquiryRequestEntity,
+    (inquiryRequest) => inquiryRequest.Image,
+    { onDelete: "CASCADE" },
+  )
+  InquiryRequest: InquiryRequestEntity;
 }
--- src/model/media/entities/inquiry-request-video.entity.ts
+++ src/model/media/entities/inquiry-request-video.entity.ts
@@ -4,8 +4,10 @@ import { MediaEntity } from "../../../common/entities/media.entity";
 
 @Entity({ name: "inquiry_request_videos", synchronize: true })
 export class InquiryRequestVideoEntity extends MediaEntity {
-  @ManyToOne(() => InquiryRequestEntity, (inquiry) => inquiry.Image, {
-    onDelete: "CASCADE",
-  })
-  Inquiry: InquiryRequestEntity;
+  @ManyToOne(
+    () => InquiryRequestEntity,
+    (inquiryRequest) => inquiryRequest.Image,
+    { onDelete: "CASCADE" },
+  )
+  InquiryRequest: InquiryRequestEntity;
 }
