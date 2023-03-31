3bf050589dd903536232061076bfd734336f804c
idooru <shere1765@gmail.com>
2023-03-31 17:34:57 +0900

    [추가] 클래스에 사용될 메서드 추가

--- src/model/inquiry/controllers/inquiry-v1-only-admin.controller.ts
+++ src/model/inquiry/controllers/inquiry-v1-only-admin.controller.ts
@@ -1,4 +1,227 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
+import {
+  mediaCookieKeys,
+  verifyCookieKeys,
+} from "src/common/config/cookie-key-configs";
+import { GetJWT } from "src/common/decorators/get.jwt.decorator";
+import { MediaCookiesParser } from "src/common/decorators/media-cookies-parser.decorator";
+import { IsAdminGuard } from "src/common/guards/authenticate/is-admin.guard";
+import { IsLoginGuard } from "src/common/guards/authenticate/is-login.guard";
+import { VerifyDataGuard } from "src/common/guards/verify/verify-data.guard";
+import { JsonClearCookiesInterface } from "src/common/interceptors/general/interface/json-clear-cookies.interface";
+import { JsonGeneralInterface } from "src/common/interceptors/general/interface/json-general-interface";
+import { JsonClearCookiesInterceptor } from "src/common/interceptors/general/json-clear-cookies.interceptor";
+import { JsonGeneralInterceptor } from "src/common/interceptors/general/json-general.interceptor";
+import { JwtAccessTokenPayload } from "src/model/auth/jwt/jwt-access-token-payload.interface";
+import { RequestMediaDto } from "src/model/media/dto/request-media.dto";
+import { InquiryResponseDto } from "../dto/response/inquiry-response.dto";
+import { InquiryResponseBundleService } from "../services/response/inquiry-response-bundle.service";
+import { InquiryResponseGeneralService } from "../services/response/inquiry-response-general.service";
+import { InquiryResponseSendMailService } from "../services/response/inquiry-response-send-mail.service";
 
+@UseGuards(IsAdminGuard)
+@UseGuards(IsLoginGuard)
 @Controller("/api/v1/only-admin/inquiry")
-export class InquiryVersionOneOnlyAdminController {}
+export class InquiryVersionOneOnlyAdminController {
+  constructor(
+    private readonly inquiryResponseGeneralService: InquiryResponseGeneralService,
+    private readonly inquiryResponseBundleService: InquiryResponseBundleService,
+    private readonly inquiryResponseSendMailService: InquiryResponseSendMailService,
+  ) {}
+
+  @UseInterceptors(JsonClearCookiesInterceptor)
+  @UseGuards(
+    new VerifyDataGuard(verifyCookieKeys.inquiry.response.is_exist.id_executed),
+  )
+  @Post(
+    "/inquiry-request/:inquiryRequestId/client-user/:clientUserId/image&video",
+  )
+  async createInquiryResponseWithImageAndVideo(
+    @Param("inquiryRequestId") inquiryRequestId: string,
+    @Param("clientUserId") clientUserId: string,
+    @MediaCookiesParser(mediaCookieKeys.inquiry.response.image_url_cookie)
+    inquiryResponseImgCookies: RequestMediaDto[],
+    @MediaCookiesParser(mediaCookieKeys.inquiry.response.video_url_cookie)
+    inquiryResponseVdoCookies: RequestMediaDto[],
+    @GetJWT() jwtPayload: JwtAccessTokenPayload,
+    @Body() inquiryResponseDto: InquiryResponseDto,
+  ): Promise<JsonClearCookiesInterface> {
+    const inquiryResponse =
+      await this.inquiryResponseGeneralService.createInquiry({
+        clientUserId,
+        inquiryRequestId,
+        inquiryResponseDto,
+        jwtPayload,
+      });
+
+    const mediaWork = async () => {
+      await this.inquiryResponseBundleService.pushInquiryMedia({
+        inquiryResponseDto,
+        inquiryResponseImgCookies,
+        inquiryResponseVdoCookies,
+      });
+
+      await this.inquiryResponseBundleService.insertInquiryMedia({
+        inquiryResponseDto,
+        inquiryResponse,
+        inquiryResponseImgCookies,
+        inquiryResponseVdoCookies,
+      });
+    };
+
+    await Promise.all([
+      mediaWork(),
+      this.inquiryResponseSendMailService.sendMailToClient({
+        clientUserId,
+        inquiryRequestId,
+      }),
+    ]);
+
+    return {
+      statusCode: 201,
+      message: "문의 응답을 생성하였습니다.",
+      cookieKey: [
+        ...inquiryResponseImgCookies.map((cookie) => cookie.whatCookie),
+        ...inquiryResponseVdoCookies.map((cookie) => cookie.whatCookie),
+      ],
+    };
+  }
+
+  @UseInterceptors(JsonClearCookiesInterceptor)
+  @UseGuards(
+    new VerifyDataGuard(verifyCookieKeys.inquiry.response.is_exist.id_executed),
+  )
+  @Post("/inquiry-request/:inquiryRequestId/client-user/:clientUserId/image")
+  async createInquiryWithImage(
+    @Param("inquiryRequestId") inquiryRequestId: string,
+    @Param("clientUserId") clientUserId: string,
+    @MediaCookiesParser(mediaCookieKeys.inquiry.response.image_url_cookie)
+    inquiryResponseImgCookies: RequestMediaDto[],
+    @GetJWT() jwtPayload: JwtAccessTokenPayload,
+    @Body() inquiryResponseDto: InquiryResponseDto,
+  ): Promise<JsonClearCookiesInterface> {
+    const inquiryResponse =
+      await this.inquiryResponseGeneralService.createInquiry({
+        clientUserId,
+        inquiryRequestId,
+        inquiryResponseDto,
+        jwtPayload,
+      });
+
+    const mediaWork = async () => {
+      await this.inquiryResponseBundleService.pushInquiryMedia({
+        inquiryResponseDto,
+        inquiryResponseImgCookies,
+      });
+
+      await this.inquiryResponseBundleService.insertInquiryMedia({
+        inquiryResponseDto,
+        inquiryResponse,
+        inquiryResponseImgCookies,
+      });
+    };
+
+    await Promise.all([
+      mediaWork(),
+      this.inquiryResponseSendMailService.sendMailToClient({
+        clientUserId,
+        inquiryRequestId,
+      }),
+    ]);
+
+    return {
+      statusCode: 201,
+      message: "문의 응답을 생성하였습니다.",
+      cookieKey: [
+        ...inquiryResponseImgCookies.map((cookie) => cookie.whatCookie),
+      ],
+    };
+  }
+
+  @UseInterceptors(JsonClearCookiesInterceptor)
+  @UseGuards(
+    new VerifyDataGuard(verifyCookieKeys.inquiry.response.is_exist.id_executed),
+  )
+  @Post("/inquiry-request/:inquiryRequestId/client-user/:clientUserId/video")
+  async createInquiryWithVideo(
+    @Param("inquiryRequestId") inquiryRequestId: string,
+    @Param("clientUserId") clientUserId: string,
+    @MediaCookiesParser(mediaCookieKeys.inquiry.response.video_url_cookie)
+    inquiryResponseVdoCookies: RequestMediaDto[],
+    @GetJWT() jwtPayload: JwtAccessTokenPayload,
+    @Body() inquiryResponseDto: InquiryResponseDto,
+  ): Promise<JsonClearCookiesInterface> {
+    const inquiryResponse =
+      await this.inquiryResponseGeneralService.createInquiry({
+        clientUserId,
+        inquiryRequestId,
+        inquiryResponseDto,
+        jwtPayload,
+      });
+
+    const mediaWork = async () => {
+      await this.inquiryResponseBundleService.pushInquiryMedia({
+        inquiryResponseDto,
+        inquiryResponseVdoCookies,
+      });
+
+      await this.inquiryResponseBundleService.insertInquiryMedia({
+        inquiryResponseDto,
+        inquiryResponse,
+        inquiryResponseVdoCookies,
+      });
+    };
+
+    await Promise.all([
+      mediaWork(),
+      this.inquiryResponseSendMailService.sendMailToClient({
+        clientUserId,
+        inquiryRequestId,
+      }),
+    ]);
+
+    return {
+      statusCode: 201,
+      message: "문의 응답을 생성하였습니다.",
+      cookieKey: [
+        ...inquiryResponseVdoCookies.map((cookie) => cookie.whatCookie),
+      ],
+    };
+  }
+
+  @UseInterceptors(JsonGeneralInterceptor)
+  @UseGuards(
+    new VerifyDataGuard(verifyCookieKeys.inquiry.response.is_exist.id_executed),
+  )
+  @Post("/inquiry-request/:inquiryRequestId/client-user/:clientUserId")
+  async createInquiryWithoutMedia(
+    @Param("inquiryRequestId") inquiryRequestId: string,
+    @Param("clientUserId") clientUserId: string,
+    @GetJWT() jwtPayload: JwtAccessTokenPayload,
+    @Body() inquiryResponseDto: InquiryResponseDto,
+  ): Promise<JsonGeneralInterface<void>> {
+    await this.inquiryResponseGeneralService.createInquiry({
+      clientUserId,
+      inquiryRequestId,
+      inquiryResponseDto,
+      jwtPayload,
+    });
+
+    await this.inquiryResponseSendMailService.sendMailToClient({
+      clientUserId,
+      inquiryRequestId,
+    });
+
+    return {
+      statusCode: 201,
+      message: "문의 응답을 생성하였습니다.",
+    };
+  }
+}
--- src/model/inquiry/controllers/inquiry-v1-verify.controller.ts
+++ src/model/inquiry/controllers/inquiry-v1-verify.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, UseInterceptors } from "@nestjs/common";
+import { verifyCookieKeys } from "src/common/config/cookie-key-configs";
+import { SendVerifyCookieInterceptor } from "src/common/interceptors/verify/send-verify-cookie.interceptor";
+import { VerifyDataDto } from "src/common/interceptors/verify/verify-data.dto";
+import { InquiryVerifyService } from "../services/inquiry-verify.service";
 
 @Controller("/api/v1/verify/inquiry")
-export class InquiryVersionOneVerifyController {}
+export class InquiryVersionOneVerifyController {
+  constructor(private readonly inquiryVerfiyService: InquiryVerifyService) {}
+
+  private readonly inquiryVerifyCookieKey = verifyCookieKeys.inquiry;
+
+  @UseInterceptors(SendVerifyCookieInterceptor)
+  @Get("/existent/id/:id")
+  async isExistInquiryRequestId(
+    @Param("id") id: string,
+  ): Promise<VerifyDataDto> {
+    await this.inquiryVerfiyService.isExistInquiryRequestId(id);
+
+    return {
+      message:
+        "해당 문의 요청 아이디가 데이터베이스에 존재하는 것이 확인되었습니다.",
+      setCookieKey: this.inquiryVerifyCookieKey.request.is_exist.id_executed,
+    };
+  }
+}
