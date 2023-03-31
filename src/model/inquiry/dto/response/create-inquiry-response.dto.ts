1c9ce9e63e68672e39663a788c52724e32156eea
idooru <shere1765@gmail.com>
2023-03-31 17:35:58 +0900

    [추가] dto 항목에 jwtPayload 추가

--- src/model/inquiry/dto/response/create-inquiry-response.dto.ts
+++ src/model/inquiry/dto/response/create-inquiry-response.dto.ts
@@ -1,3 +1,4 @@
+import { JwtAccessTokenPayload } from "src/model/auth/jwt/jwt-access-token-payload.interface";
 import { InquiryRequestEntity } from "../../entities/inquiry-request.entity";
 import { InquiryResponseDto } from "./inquiry-response.dto";
 
@@ -5,6 +6,7 @@ export class CreateInquiryResponseDto {
   inquiryRequestId: string;
   clientUserId: string;
   inquiryResponseDto: InquiryResponseDto;
+  jwtPayload: JwtAccessTokenPayload;
 }
 
 export class CreateInquiryResponseDao {
