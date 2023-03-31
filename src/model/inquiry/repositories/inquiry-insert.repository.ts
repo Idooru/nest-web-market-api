4f275b3d82ca7ea5eb06d16bc1f1cdf7052af321
idooru <shere1765@gmail.com>
2023-03-31 17:43:39 +0900

    [추가] 외래키 컬럼에 아이디를 삽입하는 메서드 추가

--- src/model/inquiry/repositories/inquiry-insert.repository.ts
+++ src/model/inquiry/repositories/inquiry-insert.repository.ts
@@ -1,22 +1,26 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { RepositoryLogger } from "src/common/classes/repository.logger";
+import { AdminUserEntity } from "src/model/user/entities/admin-user.entity";
 import { ClientUserEntity } from "src/model/user/entities/client-user.entity";
 import { Repository } from "typeorm";
 import { InquiryRequestEntity } from "../entities/inquiry-request.entity";
+import { InquiryResponseEntity } from "../entities/inquiry-response.entity";
 
 @Injectable()
 export class InquiryInsertRepository extends RepositoryLogger {
   constructor(
     @InjectRepository(InquiryRequestEntity)
-    private readonly inquiryRepository: Repository<InquiryRequestEntity>,
+    private readonly inquiryRequestRepository: Repository<InquiryRequestEntity>,
+    @InjectRepository(InquiryResponseEntity)
+    private readonly inquiryResponseRepository: Repository<InquiryResponseEntity>,
   ) {
     super("Inquiry Insert");
   }
 
   async findLastCreatedInquiryRequest(): Promise<InquiryRequestEntity> {
     try {
-      return await this.inquiryRepository
+      return await this.inquiryRequestRepository
         .createQueryBuilder()
         .select("inquiryRequest")
         .from(InquiryRequestEntity, "inquiryRequest")
@@ -29,16 +33,82 @@ export class InquiryInsertRepository extends RepositoryLogger {
     }
   }
 
-  async insertInquiryRequestIdOnClientUser(
+  async findLastCreatedInquiryResponse(): Promise<InquiryResponseEntity> {
+    try {
+      return await this.inquiryResponseRepository
+        .createQueryBuilder()
+        .select("inquiryResponse")
+        .from(InquiryResponseEntity, "inquiryResponse")
+        .orderBy("inquiryResponse.createdAt", "DESC")
+        .limit(1)
+        .getOne();
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async insertClientUserIdOnInquiryRequest(
     clientUser: ClientUserEntity,
     inquiryRequest: InquiryRequestEntity,
   ): Promise<void> {
     try {
-      await this.inquiryRepository
+      await this.inquiryRequestRepository
         .createQueryBuilder()
         .update(InquiryRequestEntity)
-        .set({ inquirer: clientUser })
+        .set({ inquiryRequestWritter: clientUser })
         .where("id = :id", { id: inquiryRequest.id })
+        .execute();
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async insertClientUserIdOnInquiryResponse(
+    clientUser: ClientUserEntity,
+    inquiryResponse: InquiryResponseEntity,
+  ): Promise<void> {
+    try {
+      await this.inquiryResponseRepository
+        .createQueryBuilder()
+        .update(InquiryResponseEntity)
+        .set({ inquiryRequestWritter: clientUser })
+        .where("id = :id", { id: inquiryResponse.id })
+        .execute();
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async insertAdminUserIdOnInquiryRequest(
+    adminUser: AdminUserEntity,
+    inquiryRequest: InquiryRequestEntity,
+  ): Promise<void> {
+    try {
+      await this.inquiryRequestRepository
+        .createQueryBuilder()
+        .update(InquiryRequestEntity)
+        .set({ inquiryResponseWritter: adminUser })
+        .where("id = :id", { id: inquiryRequest.id })
+        .execute();
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async insertAdminUserIdOnInquiryResponse(
+    adminUser: AdminUserEntity,
+    inquiryResponse: InquiryResponseEntity,
+  ): Promise<void> {
+    try {
+      await this.inquiryResponseRepository
+        .createQueryBuilder()
+        .update(InquiryResponseEntity)
+        .set({ inquiryResponseWritter: adminUser })
+        .where("id = :id", { id: inquiryResponse.id })
         .execute();
     } catch (err) {
       this.logger.error(err);
