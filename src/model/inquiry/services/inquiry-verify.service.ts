9587a9fdf5cc2fc7e33cff8c2da5199f5a25e467
idooru <shere1765@gmail.com>
2023-03-31 17:48:10 +0900

    [추가] 문의 요청 아이디가 존재하는지 검증하는 메서드 추가

--- src/model/inquiry/services/inquiry-verify.service.ts
+++ src/model/inquiry/services/inquiry-verify.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InquiryVerifyRepository } from "../repositories/inquiry-verify.repository";
 
 @Injectable()
-export class InquiryVerifyService {}
+export class InquiryVerifyService {
+  constructor(
+    private readonly inquiryVerifyRepository: InquiryVerifyRepository,
+  ) {}
+
+  async isExistInquiryRequestId(id: string): Promise<void> {
+    const result = await this.inquiryVerifyRepository.isExistInquiryRequestId(
+      id,
+    );
+
+    if (!result) {
+      throw new BadRequestException(
+        "해당 문의 요청 아이디는 데이터베이스에 존재하지 않습니다.",
+      );
+    }
+  }
+}
