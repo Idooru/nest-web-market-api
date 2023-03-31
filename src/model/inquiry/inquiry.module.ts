d8f7eb50b6a6d59daa7c9b2ac172c1df3a7cba69
idooru <shere1765@gmail.com>
2023-03-31 17:53:12 +0900

    [추가] 문의 요청, 응답 관련 서비스 레이어 추가

--- src/model/inquiry/inquiry.module.ts
+++ src/model/inquiry/inquiry.module.ts
@@ -1,9 +1,8 @@
 import { Module, forwardRef } from "@nestjs/common";
-import { InquiryGeneralService } from "./services/inquiry-general.service";
 import { InquiryVersionOneOnlyClientController } from "./controllers/inquiry-v1-only-client.controller";
 import { InquiryGeneralRepository } from "./repositories/inquiry-general.repository";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { InquiryEntity } from "src/model/inquiry/entities/inquiry.entity";
+import { InquiryRequestEntity } from "src/model/inquiry/entities/inquiry-request.entity";
 import { MediaModule } from "../media/media.module";
 import { UserModule } from "../user/user.module";
 import { ProductModule } from "../product/product.module";
@@ -13,15 +12,29 @@ import { InquiryVersionOneOnlyAdminController } from "./controllers/inquiry-v1-o
 import { InquiryVersionOneVerifyController } from "./controllers/inquiry-v1-verify.controller";
 import { InquiryVerifyService } from "./services/inquiry-verify.service";
 import { InquiryVerifyRepository } from "./repositories/inquiry-verify.repository";
+import { InquiryInsertRepository } from "./repositories/inquiry-insert.repository";
+import { MailerConfigurationModule } from "src/common/config/mailer.config";
+import { DotenvConfigurationModule } from "src/common/config/dotenv.config";
+import { InquiryRequestAccessoryService } from "./services/request/inquiry-request-accessory.service";
+import { InquiryRequestBundleService } from "./services/request/inquiry-request-bundle.service";
+import { InquiryRequestGeneralService } from "./services/request/inquiry-request-general.service";
+import { InquiryRequestSendMailService } from "./services/request/inquiry-request-send-mail.service";
+import { InquiryResponseAccessoryService } from "./services/response/inquiry-response-accessory.service";
+import { InquiryResponseBundleService } from "./services/response/inquiry-response-bundle.service";
+import { InquiryResponseGeneralService } from "./services/response/inquiry-response-general.service";
+import { InquiryResponseSendMailService } from "./services/response/inquiry-response-send-mail.service";
+import { InquiryResponseEntity } from "./entities/inquiry-response.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([InquiryEntity]),
+    TypeOrmModule.forFeature([InquiryRequestEntity, InquiryResponseEntity]),
     forwardRef(() => MediaModule),
     forwardRef(() => UserModule),
     forwardRef(() => ProductModule),
     JwtModule,
     LibraryModule,
+    MailerConfigurationModule,
+    DotenvConfigurationModule,
   ],
   controllers: [
     InquiryVersionOneOnlyClientController,
@@ -29,16 +42,32 @@ import { InquiryVerifyRepository } from "./repositories/inquiry-verify.repositor
     InquiryVersionOneVerifyController,
   ],
   providers: [
-    InquiryGeneralService,
-    InquiryGeneralRepository,
+    InquiryRequestAccessoryService,
+    InquiryRequestBundleService,
+    InquiryRequestGeneralService,
+    InquiryRequestSendMailService,
+    InquiryResponseAccessoryService,
+    InquiryResponseBundleService,
+    InquiryResponseGeneralService,
+    InquiryResponseSendMailService,
     InquiryVerifyService,
+    InquiryGeneralRepository,
     InquiryVerifyRepository,
+    InquiryInsertRepository,
   ],
   exports: [
-    InquiryGeneralService,
-    InquiryGeneralRepository,
+    InquiryRequestAccessoryService,
+    InquiryRequestBundleService,
+    InquiryRequestGeneralService,
+    InquiryRequestSendMailService,
+    InquiryResponseAccessoryService,
+    InquiryResponseBundleService,
+    InquiryResponseGeneralService,
+    InquiryResponseSendMailService,
     InquiryVerifyService,
+    InquiryGeneralRepository,
     InquiryVerifyRepository,
+    InquiryInsertRepository,
   ],
 })
 export class InquiryModule {}
