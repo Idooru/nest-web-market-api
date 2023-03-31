75e8fed23d1da39e448ccc3514123631459849ae
idooru <shere1765@gmail.com>
2023-03-31 17:44:47 +0900

    [추가] 문의 요청 아이디가 존재하는지 검증하는 메서드 추가

--- src/model/inquiry/repositories/inquiry-verify.repository.ts
+++ src/model/inquiry/repositories/inquiry-verify.repository.ts
@@ -1,9 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { RepositoryLogger } from "src/common/classes/repository.logger";
+import { Repository } from "typeorm";
+import { InquiryRequestEntity } from "../entities/inquiry-request.entity";
 
 @Injectable()
 export class InquiryVerifyRepository extends RepositoryLogger {
-  constructor() {
+  constructor(
+    @InjectRepository(InquiryRequestEntity)
+    private readonly inquiryRequestRepository: Repository<InquiryRequestEntity>,
+  ) {
     super("Inquiry Verify");
+  }
+
+  async isExistInquiryRequestId(id: string): Promise<boolean> {
+    try {
+      const result = await this.inquiryRequestRepository.exist({
+        where: { id },
+      });
+      return result ? true : false;
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 }
