23033111bc5d8c6adeb6554358141baf07854b84
idooru <shere1765@gmail.com>
2023-04-04 16:07:02 +0900

    [제거] 사용되지 않은 클래스 제거

--- src/common/classes/repository.logger.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from "@nestjs/common";
-
-export abstract class RepositoryLogger {
-  constructor(private readonly repository: string) {}
-
-  protected readonly logger = new Logger(`${this.repository} Repository`);
-}
