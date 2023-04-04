59d66c45d98cc83f1025ab12c9d0dbcc0cbe9bad
idooru <shere1765@gmail.com>
2023-04-04 23:16:22 +0900

    [수정] stuffMean 비교 대상 문구 수정

--- src/model/inquiry/error/inquiry-response-error.case.ts
+++ src/model/inquiry/error/inquiry-response-error.case.ts
@@ -4,7 +4,7 @@ export class InquiryResponseErrorCase {
   static init(error: Error, stuff: string, stuffMean: string): void {
     if (
       error.message.includes("Could not find any entity of type") &&
-      stuffMean === "id"
+      stuffMean === "아이디"
     ) {
       throw new NotFoundException(
         `해당 id(${stuff})을 가진 문의 응답을 찾을 수 없습니다.`,
