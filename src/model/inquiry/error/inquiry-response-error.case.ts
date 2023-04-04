7c5c6f4af9986426b23c0e9de38d5e2af087f85f
idooru <shere1765@gmail.com>
2023-04-04 16:04:35 +0900

    [생성] 문의 응답과 관련된 데이터베이스 작업을 할 때 발생 가능한 에러를 모아놓은 case 생성

---
+++ src/model/inquiry/error/inquiry-response-error.case.ts
@@ -0,0 +1,14 @@
+import { NotFoundException } from "@nestjs/common";
+
+export class InquiryResponseErrorCase {
+  static init(error: Error, stuff: string, stuffMean: string): void {
+    if (
+      error.message.includes("Could not find any entity of type") &&
+      stuffMean === "id"
+    ) {
+      throw new NotFoundException(
+        `해당 id(${stuff})을 가진 문의 응답을 찾을 수 없습니다.`,
+      );
+    }
+  }
+}
