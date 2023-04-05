46c39f873dbb0bb87deeb97f2c47d206083533fc
idooru <shere1765@gmail.com>
2023-04-05 17:28:41 +0900

    [추가] send mail error handle library 추가

--- src/common/lib/library.module.ts
+++ src/common/lib/library.module.ts
@@ -5,21 +5,24 @@ import { MeidaLoggerLibrary } from "./media-logger.library";
 import { SecurityLibrary } from "./security.library";
 import { TimeLoggerLibrary } from "./time-logger.library";
 import { RepositoryErrorHandleLibrary } from "./repository-error-handler.library";
+import { SendMailErrorHandlerLibrary } from "./send-mail-error-handler.library";
 
 @Module({
   imports: [DotenvConfigurationModule],
   providers: [
     TimeLoggerLibrary,
-    SecurityLibrary,
     ConfigService,
+    SecurityLibrary,
     MeidaLoggerLibrary,
     RepositoryErrorHandleLibrary,
+    SendMailErrorHandlerLibrary,
   ],
   exports: [
     TimeLoggerLibrary,
     SecurityLibrary,
     MeidaLoggerLibrary,
     RepositoryErrorHandleLibrary,
+    SendMailErrorHandlerLibrary,
   ],
 })
 export class LibraryModule {}
