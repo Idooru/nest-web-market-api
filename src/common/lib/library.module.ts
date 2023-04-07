dcee21a2774fa2a93ebc891fff4b192dc9f68d8a
idooru <shere1765@gmail.com>
2023-04-07 17:51:48 +0900

    [수정] 병합된 에러 핸들러를 사용

--- src/common/lib/library.module.ts
+++ src/common/lib/library.module.ts
@@ -4,29 +4,26 @@ import { DotenvConfigurationModule } from "../config/dotenv.config";
 import { MeidaLoggerLibrary } from "./logger/media-logger.library";
 import { SecurityLibrary } from "./config/security.library";
 import { TimeLoggerLibrary } from "./logger/time-logger.library";
-import { RepositoryLayerErrorHandleLibrary } from "./error-handler/repository-error-handler.library";
-import { ServiceLayerErrorHandlerLibrary } from "./error-handler/service-layer-error-handler.library";
 import { SendEmailLibrary } from "./email/send-email-library";
 import { MailerConfigurationModule } from "../config/mailer.config";
+import { ErrorHandlerBuilder } from "./error-handler/error-hanlder-builder";
 
 @Module({
   imports: [DotenvConfigurationModule, MailerConfigurationModule],
   providers: [
-    TimeLoggerLibrary,
     ConfigService,
     SecurityLibrary,
-    MeidaLoggerLibrary,
-    RepositoryLayerErrorHandleLibrary,
-    ServiceLayerErrorHandlerLibrary,
     SendEmailLibrary,
+    ErrorHandlerBuilder,
+    TimeLoggerLibrary,
+    MeidaLoggerLibrary,
   ],
   exports: [
-    TimeLoggerLibrary,
     SecurityLibrary,
-    MeidaLoggerLibrary,
-    RepositoryLayerErrorHandleLibrary,
-    ServiceLayerErrorHandlerLibrary,
     SendEmailLibrary,
+    ErrorHandlerBuilder,
+    TimeLoggerLibrary,
+    MeidaLoggerLibrary,
   ],
 })
 export class LibraryModule {}
