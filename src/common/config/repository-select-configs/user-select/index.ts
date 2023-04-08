c19a12707df29c1401404ee7321c7d2d6d673d11
idooru <shere1765@gmail.com>
2023-04-08 18:49:56 +0900

    [추가] user select prpperty 객체에 타입 추가

--- src/common/config/repository-select-configs/user-select/index.ts
+++ src/common/config/repository-select-configs/user-select/index.ts
@@ -1,4 +1,14 @@
-export const userSelectProperty = {
+export interface UserSelectProperty {
+  userBaseSelect: string[];
+  clientUserSelect: string[];
+  clientUserSimpleSelect: string[];
+  clientUserProfileSelect: string[];
+  adminUserSelect: string[];
+  adminUserProfileSelect: string[];
+  whenAdminSelectClientUser: string[];
+}
+
+export const userSelectProperty: UserSelectProperty = {
   userBaseSelect: ["user", "Profile", "Auth"],
   clientUserSelect: ["user", "Profile", "Auth", "Client"],
   clientUserSimpleSelect: ["user", "Auth.nickname", "Auth.email"],
