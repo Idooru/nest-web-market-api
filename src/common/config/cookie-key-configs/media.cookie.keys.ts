de4458eb0471780fe5bf60f45dc71ac2d7f73905
idooru <shere1765@gmail.com>
2023-04-09 23:07:24 +0900

    [수정] 쿠키 키와 관련된 객체를 각각 파일로 이동

---
+++ src/common/config/cookie-key-configs/media.cookie.keys.ts
@@ -0,0 +1,19 @@
+export const mediaCookieKeys = {
+  product: {
+    image_url_cookie: "product_image_url_cookie",
+  },
+  review: {
+    image_url_cookie: "review_image_url_cookie",
+    video_url_cookie: "review_video_url_cookie",
+  },
+  inquiry: {
+    request: {
+      image_url_cookie: "inquiry_request_image_url_cookie",
+      video_url_cookie: "inquiry_request_video_url_cookie",
+    },
+    response: {
+      image_url_cookie: "inquiry_response_image_url_cookie",
+      video_url_cookie: "inquiry_response_video_url_cookie",
+    },
+  },
+};
