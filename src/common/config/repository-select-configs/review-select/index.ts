170db43b4886df57c5ffe09d6b050444926d25b9
idooru <shere1765@gmail.com>
2023-04-09 22:33:16 +0900

    [추가] review select property 객체에 타입 추가

--- src/common/config/repository-select-configs/review-select/index.ts
+++ src/common/config/repository-select-configs/review-select/index.ts
@@ -1,3 +1,8 @@
+export interface ReviewSelectProperty {
+  reviewsSelect: string[];
+  starRateSelect: string[];
+}
+
 export const reviewSelectProperty = {
   reviewsSelect: ["review", "Product", "Client", "Image", "Video"],
   starRateSelect: ["starRate"],
