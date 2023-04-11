6e5c56b7473253661e3dc3fb0e2050daa3dabdfb
idooru <shere1765@gmail.com>
2023-04-11 23:49:42 +0900

    [생성] error case 클래스에서 공통적으로 사용될 프로퍼티를 사용하기 위한 error case prop 생성

---
+++ src/common/classes/error-case-prop.ts
@@ -0,0 +1,14 @@
+export class ErrorCaseProp {
+  constructor(
+    protected readonly stuffs: string[],
+    protected readonly stuffMeans: string[],
+  ) {}
+
+  protected readonly stuffArr = [this.stuffMeans, this.stuffs].map((_, idx) => {
+    return this.makeObjLiteral(this.stuffs[idx], this.stuffMeans[idx]);
+  });
+
+  private makeObjLiteral(stuff: string, stuffMean: string) {
+    return { [stuffMean]: stuff };
+  }
+}
