7b0df24bdacb30918126a4940b668c34e72263a8
idooru <shere1765@gmail.com>
2023-04-12 17:03:54 +0900

    [추가] 에러 유발 사유를 초기화 시키는 메서드 추가

--- src/common/classes/error-case-prop.ts
+++ src/common/classes/error-case-prop.ts
@@ -1,12 +1,16 @@
-export class ErrorCaseProp {
-  constructor(
-    protected readonly stuffs: string[],
-    protected readonly stuffMeans: string[],
-  ) {}
+import { ErrorHandlerBuilder } from "../lib/error-handler/error-hanlder-builder";
 
-  protected readonly stuffArr = [this.stuffMeans, this.stuffs].map((_, idx) => {
+export class ErrorCaseProp {
+  constructor(protected stuffs: string[], protected stuffMeans: string[]) {}
+
+  protected stuffArr = [this.stuffMeans, this.stuffs].map((_, idx) => {
     return this.makeObjLiteral(this.stuffs[idx], this.stuffMeans[idx]);
   });
+
+  public static clearStuffs() {
+    ErrorHandlerBuilder.stuffs = [];
+    ErrorHandlerBuilder.stuffMeans = [];
+  }
 
   private makeObjLiteral(stuff: string, stuffMean: string) {
     return { [stuffMean]: stuff, key: () => stuffMean, value: () => stuff };
