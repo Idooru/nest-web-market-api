a1b8924e48a44c678e8c451c8097471a04d79db3
idooru <shere1765@gmail.com>
2023-04-17 12:59:23 +0900

    [수정] 각각의 실행 환경으로부터 .env 경로를 설정

--- src/common/config/dotenv.config.ts
+++ src/common/config/dotenv.config.ts
@@ -3,11 +3,22 @@ import { ConfigModule } from "@nestjs/config";
 import { config } from "dotenv";
 config();
 
+const returnEnviromentPath = () => {
+  switch (process.env.NODE_ENV) {
+    case "dev":
+      return "dev";
+    case "prod":
+      return "prod";
+    case "test":
+      return "test";
+  }
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env${process.env.NODE_ENV === "dev" ? ".dev" : ".test"}`,
+      envFilePath: `.env.${returnEnviromentPath()}`,
     }),
   ],
 })
