ae1448bcd413d382f5b1c663c07519143259b830
idooru <shere1765@gmail.com>
2023-05-04 23:49:33 +0900

    [제거] stuffs를 비우는 메서드

--- src/common/classes/error-case-prop.ts
+++ src/common/classes/error-case-prop.ts
@@ -1,5 +1,3 @@
-import { ErrorHandlerBuilder } from "../lib/error-handler/error-hanlder.builder";
-
 export class ErrorCaseProp {
   constructor(
     protected readonly stuffs: string[],
@@ -9,11 +7,6 @@ export class ErrorCaseProp {
   protected stuffArr = [this.stuffMeans, this.stuffs].map((_, idx) => {
     return this.makeObjLiteral(this.stuffs[idx], this.stuffMeans[idx]);
   });
-
-  public static clearStuffs() {
-    ErrorHandlerBuilder.stuffs = [];
-    ErrorHandlerBuilder.stuffMeans = [];
-  }
 
   private makeObjLiteral(stuff: string, stuffMean: string) {
     return { [stuffMean]: stuff, key: () => stuffMean, value: () => stuff };
