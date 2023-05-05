fbf5e9b672a6da9051aa1c55e554f14d50accf22
idooru <shere1765@gmail.com>
2023-05-05 12:57:43 +0900

    [생성] TypeOrm Error Handler 빌더

---
+++ src/common/lib/error-handler/typeorm-error-handler.builder.ts
@@ -0,0 +1,44 @@
+import { EntityTarget, TypeORMError } from "typeorm";
+import { TypeOrmErrorHandler } from "./typeorm-error-handler.library";
+
+export class TypeOrmErrorHandlerBuilder {
+  private entity: EntityTarget;
+  private error: TypeORMError;
+  private className: string;
+  private methodName: string;
+  public static stuffs: string[] = [];
+  public static stuffMeans: string[] = [];
+
+  public setEntity(entity: EntityTarget): this {
+    this.entity = entity;
+    return this;
+  }
+
+  public setError(error: TypeORMError): this {
+    this.error = error;
+    return this;
+  }
+
+  public setSourceNames(className: string, methodName: string): this {
+    this.className = className;
+    this.methodName = methodName;
+    return this;
+  }
+
+  public setStuffs(stuff: string, stuffMean: string): this {
+    TypeOrmErrorHandlerBuilder.stuffs.push(stuff);
+    TypeOrmErrorHandlerBuilder.stuffMeans.push(stuffMean);
+    return this;
+  }
+
+  public handle() {
+    return new TypeOrmErrorHandler(
+      this.entity,
+      this.error,
+      this.className,
+      this.methodName,
+      TypeOrmErrorHandlerBuilder.stuffs,
+      TypeOrmErrorHandlerBuilder.stuffMeans,
+    );
+  }
+}
