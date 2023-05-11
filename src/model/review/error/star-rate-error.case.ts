c35f4bc83071875a61c17c76fc9265cffb80bff7
idooru <shere1765@gmail.com>
2023-05-11 23:03:10 +0900

    [수정] EntityErrorHandler를 상속하여 클래스를 strategy 패턴으로 만듦

--- src/model/review/error/star-rate-error.case.ts
+++ src/model/review/error/star-rate-error.case.ts
@@ -1,14 +1,31 @@
 import { NotFoundException } from "@nestjs/common";
+import { EntityErrorHandler } from "src/common/classes/abstract/entity-error-handler";
+import { TypeORMError } from "typeorm";
 
-export class StarRateErrorCase {
-  static init(error: Error, stuffs: string[], stuffMeans: string[]): void {
-    // if (
-    //   error.message.includes("Could not find any entity of type") &&
-    //   stuffMean === "id"
-    // ) {
-    //   throw new NotFoundException(
-    //     `해당 아이디(${stuff})의 별점을 찾을 수 없습니다. 검증 API를 먼저 사용해주세요.`,
-    //   );
-    // }
+export class StarRateErrorCase extends EntityErrorHandler {
+  constructor(
+    protected error: TypeORMError,
+    protected stuffs: string[],
+    protected stuffMeans: string[],
+  ) {
+    super(stuffs, stuffMeans);
+    this.handle(error);
+  }
+
+  public handle(error: TypeORMError): void {
+    this.notFound(error);
+  }
+
+  private notFound(error: TypeORMError): void {
+    const idStuff = this.stuffArr.find((val) => val.key() === "id");
+
+    if (
+      error.message.includes("Could not find any entity of type") &&
+      idStuff
+    ) {
+      throw new NotFoundException(
+        `해당 아이디(${idStuff.value()})의 별점을 찾을 수 없습니다. 검증 API를 먼저 사용해주세요.`,
+      );
+    }
   }
 }
