a2806b8b7539f5687db0765f0f8ec929b1c22fc2
idooru <shere1765@gmail.com>
2023-05-12 14:33:22 +0900

    [추가] 반환 타입

--- src/model/user/services/user-accessory.service.ts
+++ src/model/user/services/user-accessory.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { UserGeneralRepository } from "../repositories/user-general.repository";
+import { UserEntity } from "../entities/user.entity";
 
 @Injectable()
 export class UserAccessoryService {
   constructor(private readonly userGeneralRepository: UserGeneralRepository) {}
 
-  async findUserWithEmail(email: string) {
+  async findUserWithEmail(email: string): Promise<UserEntity> {
     return await this.userGeneralRepository.findUserWithEmail(email);
   }
 }
