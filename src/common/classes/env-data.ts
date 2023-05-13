f606c5492d4e2417743db3b5aa4befb89c6bf91b
idooru <shere1765@gmail.com>
2023-05-13 22:05:38 +0900

    [생성] Env Data 클래스

---
+++ src/common/classes/env-data.ts
@@ -0,0 +1,34 @@
+import { ConfigService } from "@nestjs/config";
+
+export class EnvData {
+  private envValues: { [x: string]: any }[];
+
+  constructor(private readonly envKeys: string[]) {
+    this.envValues = this.envKeys.map((key: string) => {
+      const value = new ConfigService().get(key);
+      return {
+        [key]: value,
+        getKeyName(): string {
+          return key;
+        },
+        getValue(): string {
+          return value;
+        },
+      };
+    });
+  }
+
+  public getValue(keyName: string): string {
+    const result = this.envValues
+      .find((value) => {
+        return value.getKeyName().toLowerCase().includes(keyName);
+      })
+      .getValue();
+
+    if (!result) {
+      throw new Error(`옳바르지 않은 키 이름(${keyName})입니다.`);
+    }
+
+    return result;
+  }
+}
