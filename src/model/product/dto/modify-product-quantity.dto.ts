663f6f46e0038a3d7553a815877b520fd75a88ac
idooru <shere1765@gmail.com>
2023-05-16 23:09:37 +0900

    [수정] 클래스 이름

--- src/model/product/dto/modify-product-quantity.dto.ts
+++ src/model/product/dto/modify-product-quantity.dto.ts
@@ -1,6 +1,6 @@
 import { PickType } from "@nestjs/swagger";
 import { ProductEntity } from "../entities/product.entity";
 
-export class ModifyProductQuantity extends PickType(ProductEntity, [
+export class ModifyProductQuantityDto extends PickType(ProductEntity, [
   "quantity",
 ] as const) {}
