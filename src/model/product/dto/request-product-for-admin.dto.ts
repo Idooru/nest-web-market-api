9b3a8c167d214a8ca145b822eae54da05bb68ed5
idooru <shere1765@gmail.com>
2023-05-16 22:45:46 +0900

    [제거] 사용되지 않은 클래스

--- src/model/product/dto/request-product-for-admin.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PickType } from "@nestjs/swagger";
-import { ProductEntity } from "../entities/product.entity";
-
-export class RequestProductForAdminDto extends PickType(ProductEntity, [
-  "id",
-  "name",
-  "type",
-] as const) {}
