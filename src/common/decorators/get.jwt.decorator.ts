f1d95d22357246416c40d2e4a402ebaa1e5b037c
idooru <shere1765@gmail.com>
2023-05-22 23:08:40 +0900

    [추가] Request 타입 캐스팅

--- src/common/decorators/get.jwt.decorator.ts
+++ src/common/decorators/get.jwt.decorator.ts
@@ -1,11 +1,15 @@
 import { createParamDecorator } from "@nestjs/common";
 import { ArgumentsHost } from "@nestjs/common";
+import { Request } from "express";
 import { JwtAccessTokenPayload } from "src/model/auth/jwt/jwt-access-token-payload.interface";
 import { JwtRefreshTokenPayload } from "src/model/auth/jwt/jwt-refresh-token-payload.interface";
 
 export const GetJWT = createParamDecorator(
-  (context: ArgumentsHost): JwtAccessTokenPayload | JwtRefreshTokenPayload => {
-    const req = context.switchToHttp().getRequest();
+  (
+    data,
+    context: ArgumentsHost,
+  ): JwtAccessTokenPayload | JwtRefreshTokenPayload => {
+    const req = context.switchToHttp().getRequest<Request>();
 
     return req.user;
   },
