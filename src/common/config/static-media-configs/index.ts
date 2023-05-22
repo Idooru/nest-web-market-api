d94fcf02e3a15f091909c5ac4ccbc48a476bd4bf
idooru <shere1765@gmail.com>
2023-05-22 13:52:11 +0900

    [생성] static media configs 객체 리터럴

---
+++ src/common/config/static-media-configs/index.ts
@@ -0,0 +1,34 @@
+export const staticMediaConfigs = [
+  {
+    mediaType: "images",
+    model: "product",
+  },
+  {
+    mediaType: "images",
+    model: "review",
+  },
+  {
+    mediaType: "videos",
+    model: "review",
+  },
+  {
+    mediaType: "images",
+    model: "inquiry",
+    others: "request",
+  },
+  {
+    mediaType: "videos",
+    model: "inquiry",
+    others: "request",
+  },
+  {
+    mediaType: "images",
+    model: "inquiry",
+    others: "response",
+  },
+  {
+    mediaType: "videos",
+    model: "inquiry",
+    others: "response",
+  },
+];
