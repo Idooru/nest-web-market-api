7d8ea97d436f6adddb8511bbafb376fa1bf39e7f
idooru <shere1765@gmail.com>
2023-05-23 22:15:39 +0900

    [추가] 에러 처리 로직

--- src/common/lib/error-handler/jwt-error-handler.library.ts
+++ src/common/lib/error-handler/jwt-error-handler.library.ts
@@ -1,26 +1,61 @@
 import { ErrorLogger } from "src/common/classes/abstract/error-logger";
-import { Throwable } from "./interface/throwable.interface";
 import { JsonWebTokenError } from "jsonwebtoken";
-import { JwtServiceException } from "src/common/errors/jwt-service.exception";
 import { JwtWhatToken } from "src/model/auth/jwt/jwt-what-token.type";
+import { Injectable } from "@nestjs/common";
+import { JwtServiceException } from "src/common/errors/jwt-service.exception";
 
-export class JwtErrorHandler extends ErrorLogger implements Throwable {
+@Injectable()
+export class JwtErrorHandler extends ErrorLogger {
   constructor(
     protected readonly error: JsonWebTokenError,
     protected readonly className: string,
     protected readonly methodName: string,
-    protected readonly whatToken: JwtWhatToken,
-    protected readonly token: string,
+    private readonly whatToken: JwtWhatToken,
   ) {
     super(error, className, methodName);
     this.main();
   }
 
-  private main() {
+  private info: string;
+
+  private main(): void {
     super.logging();
+
+    if (this.whatToken) {
+      // whatToken 프로퍼티를 확인하는 경우는 토큰을 검증 할 경우이다.
+      this.whatToken === "access_token"
+        ? this.setInfoAccessToken()
+        : this.setInfoRefreshToken();
+    } else {
+      // 토큰을 생성하는 경우이다.
+      this.info = "토큰을 생성할 때 사용한 옵션 값을 확인하세요.";
+    }
+
+    this.throwJwtServiceException();
   }
 
-  throwException(): never {
-    throw new JwtServiceException(this.error, this.whatToken, this.token);
+  private setInfoAccessToken(): void {
+    if (this.error.name.includes("Expired")) {
+      this.info =
+        "만료된 access_token입니다. refresh_token을 이용하여 토큰을 재발급 받거나 다시 로그인 해주세요.";
+    } else {
+      this.setInfoInvalidToken();
+    }
+  }
+
+  private setInfoRefreshToken(): void {
+    if (this.error.name.includes("Expired")) {
+      this.info = "만료된 refresh_token입니다. 다시 로그인 해주세요.";
+    } else {
+      this.setInfoInvalidToken();
+    }
+  }
+
+  private setInfoInvalidToken(): void {
+    this.info = "유효하지 않은 토큰입니다.";
+  }
+
+  private throwJwtServiceException(): never {
+    throw new JwtServiceException(this.info, this.error);
   }
 }
