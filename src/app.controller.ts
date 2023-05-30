df5da3d070717ce037a627c69a294d490fdca3de
Idooru <shere1765@gmail.com>
2023-05-30 16:27:08 +0900

    [생성] App Controller 클래스

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, UseInterceptors } from "@nestjs/common";
+import { JsonGeneralInterceptor } from "./common/interceptors/general/json-general.interceptor";
+import { JsonGeneralInterface } from "./common/interceptors/interface/json-general-interface";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+
+@ApiTags("공용 App Api")
+@Controller("/")
+export class AppController {
+  @ApiOperation({
+    summary: "main page",
+    description: "어플리케이션의 메인 페이지 입니다.",
+  })
+  @UseInterceptors(JsonGeneralInterceptor)
+  @Get("/")
+  mainPage(): JsonGeneralInterface<void> {
+    return {
+      statusCode: 200,
+      message: "메인 페이지 입니다.",
+    };
+  }
+}
