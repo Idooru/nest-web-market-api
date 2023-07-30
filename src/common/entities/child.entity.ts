1d8b4dfa0eee1743aa6c41116f9e92c24b44572b
Idooru <shere1765@gmail.com>
2023-07-30 16:13:47 +0900

    [생성] Child Entity 클래스

---
+++ src/common/entities/child.entity.ts
@@ -0,0 +1,9 @@
+import { IsUUID } from "class-validator";
+import { DateEntity } from "./date.entity";
+import { PrimaryColumn } from "typeorm";
+
+export abstract class ChildEntity extends DateEntity {
+  @IsUUID()
+  @PrimaryColumn("uuid")
+  id: string;
+}
