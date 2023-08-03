fafe5b4ea69ade43e2f02d02a912ed79a3009785
Idooru <shere1765@gmail.com>
2023-08-03 18:47:44 +0900

    [수정] 상속된 클래스 이름

--- src/common/entities/media.entity.ts
+++ src/common/entities/media.entity.ts
@@ -1,8 +1,8 @@
 import { IsEmail, IsString, IsUrl } from "class-validator";
 import { Column } from "typeorm";
-import { ChildEntity } from "./child.entity";
+import { CommonEntity } from "./common.entity";
 
-export abstract class MediaEntity extends ChildEntity {
+export abstract class MediaEntity extends CommonEntity {
   @IsUrl()
   @IsString()
   @Column({ type: "varchar", nullable: false, unique: true })
