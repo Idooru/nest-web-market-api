d2f8a9c61ad8a38528262f3b9686d5eb56a166bc
Idooru <shere1765@gmail.com>
2023-08-03 15:29:22 +0900

    [수정] 조건문 제거

--- src/common/guards/verify/verify-data.guard.ts
+++ src/common/guards/verify/verify-data.guard.ts
@@ -12,40 +12,37 @@ export class VerifyDataGuard extends ErrorHandlerProps implements CanActivate {
   }
 
   canActivate(context: ExecutionContext): boolean {
-    if (process.env.NODE_ENV !== "dev") {
-      const req = context.switchToHttp().getRequest<Request>();
-      const res = context.switchToHttp().getResponse<Response>();
+    const req = context.switchToHttp().getRequest<Request>();
+    const res = context.switchToHttp().getResponse<Response>();
 
-      const needCookies = this.data;
-      const importedCookies = Object.keys(req.signedCookies).filter((cookie) =>
-        needCookies.includes(cookie),
+    const needCookies = this.data;
+    const importedCookies = Object.keys(req.signedCookies).filter((cookie) =>
+      needCookies.includes(cookie),
+    );
+
+    const sortedNeedCookies = needCookies.sort();
+    const sortedImportedCookies = importedCookies.sort();
+
+    const isEqual =
+      needCookies.length === importedCookies.length &&
+      sortedNeedCookies.every(
+        (cookieKey, idx) => cookieKey === sortedImportedCookies[idx],
       );
 
-      const sortedNeedCookies = needCookies.sort();
-      const sortedImportedCookies = importedCookies.sort();
-
-      const isEqual =
-        needCookies.length === importedCookies.length &&
-        sortedNeedCookies.every(
-          (cookieKey, idx) => cookieKey === sortedImportedCookies[idx],
-        );
-
-      if (!isEqual) {
-        this.methodName = this.canActivate.name;
-        new HttpExceptionHandlingBuilder()
-          .setMessage(
-            "검증에 필요한 쿠키가 일치하지 않았기 때문에 해당 로직을 사용할 수 없습니다. 검증 API를 먼저 실행시켜 주세요.",
-          )
-          .setOccuredLocation("class")
-          .setOccuredClass(this.className, this.methodName)
-          .setExceptionStatus(HttpStatus.UNAUTHORIZED)
-          .handle();
-      }
-
-      importedCookies.forEach((cookie) => res.clearCookie(cookie));
-
-      return true;
+    if (!isEqual) {
+      this.methodName = this.canActivate.name;
+      new HttpExceptionHandlingBuilder()
+        .setMessage(
+          "검증에 필요한 쿠키가 일치하지 않았기 때문에 해당 로직을 사용할 수 없습니다. 검증 API를 먼저 실행시켜 주세요.",
+        )
+        .setOccuredLocation("class")
+        .setOccuredClass(this.className, this.methodName)
+        .setExceptionStatus(HttpStatus.UNAUTHORIZED)
+        .handle();
     }
+
+    importedCookies.forEach((cookie) => res.clearCookie(cookie));
+
     return true;
   }
 }
