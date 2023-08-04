857abd6521900288cf0d1817826c001730f60d8d
Idooru <shere1765@gmail.com>
2023-08-04 16:27:53 +0900

    [추가] insert Product Id On Admin User 메서드

--- src/model/product/interfaces/repositories/product-insert-repository.interface.ts
+++ src/model/product/interfaces/repositories/product-insert-repository.interface.ts
@@ -1,5 +1,10 @@
+import { AdminUserEntity } from "src/model/user/entities/admin-user.entity";
 import { ProductEntity } from "../../entities/product.entity";
 
 export interface IProductInsertRepository {
   findOneProductById(id: string): Promise<ProductEntity>;
+  insertProductIdOnAdminUser(
+    adminUser: AdminUserEntity,
+    product: ProductEntity,
+  ): Promise<void>;
 }
--- src/model/product/repositories/product-insert.repository.ts
+++ src/model/product/repositories/product-insert.repository.ts
@@ -6,6 +6,7 @@ import { ErrorHandlerProps } from "src/common/classes/abstract/error-handler-pro
 import { TypeOrmErrorHandlingBuilder } from "src/common/lib/error-handler/typeorm-error-handling.builder";
 import { IProductInsertRepository } from "../interfaces/repositories/product-insert-repository.interface";
 import { ProductErrorHandler } from "../error/product-error.handler";
+import { AdminUserEntity } from "src/model/user/entities/admin-user.entity";
 
 @Injectable()
 export class ProductInsertRepository
@@ -30,6 +31,27 @@ export class ProductInsertRepository
         .getOneOrFail();
     } catch (err) {
       this.methodName = this.findOneProductById.name;
+      this.typeOrmErrorHandlerBuilder
+        .setErrorHandler(ProductErrorHandler)
+        .setError(err)
+        .setSourceNames(this.className, this.methodName)
+        .handle();
+    }
+  }
+
+  async insertProductIdOnAdminUser(
+    adminUser: AdminUserEntity,
+    product: ProductEntity,
+  ): Promise<void> {
+    try {
+      await this.productRepository
+        .createQueryBuilder()
+        .update(ProductEntity)
+        .set({ creater: adminUser })
+        .where("id = :id", { id: product.id })
+        .execute();
+    } catch (err) {
+      this.methodName = this.insertProductIdOnAdminUser.name;
       this.typeOrmErrorHandlerBuilder
         .setErrorHandler(ProductErrorHandler)
         .setError(err)
