b9e0abb3966ed55578c13effc993e0f76fe50797
Idooru <shere1765@gmail.com>
2023-09-02 13:35:14 +0900

    [제거] 불필요한 클래스

--- src/model/user/dtos/create-user-base.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserAuthEntity } from "../entities/user-auth.entity";
-import { UserProfileEntity } from "../entities/user-profile.entity";
-
-export class CreateUserBaseDto {
-  Profile: UserProfileEntity;
-  Auth: UserAuthEntity;
-  type: ["client", "admin"];
-}
