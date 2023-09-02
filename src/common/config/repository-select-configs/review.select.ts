6f67e94e2cf0180d743e09e3649343a9810aee5f
Idooru <shere1765@gmail.com>
2023-09-02 10:54:57 +0900

    [추가] Review 엔티티 select 목록

--- src/common/config/repository-select-configs/review.select.ts
+++ src/common/config/repository-select-configs/review.select.ts
@@ -1,9 +1,11 @@
 export interface ReviewSelectProperty {
+  review: string[];
   reviews: string[];
   starRate: string[];
 }
 
 export const reviewSelectProperty = {
+  review: ["review", "Product", "Image", "Video"],
   reviews: ["review", "Product", "Client", "Image", "Video"],
   starRate: ["starRate"],
 };
