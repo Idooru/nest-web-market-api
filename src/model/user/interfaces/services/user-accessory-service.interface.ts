3e48e4fe5433126fad255f6363441dc62e365480
Idooru <shere1765@gmail.com>
2023-09-09 23:33:15 +0900

    [제거] 불필요한 파일

--- src/model/user/interfaces/services/user-accessory-service.interface.ts
+++
@@ -1,5 +0,0 @@
-import { UserEntity } from "../../entities/user.entity";
-
-export interface IUserAccessoryService {
-  findUserWithEmail(email: string): Promise<UserEntity>;
-}
--- src/model/user/interfaces/services/user-verify-service.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IUserVerifyService {
-  isExistUserId(id: string): Promise<void>;
-  isExistUserEmail(email: string): Promise<void>;
-  isNotExistUserEmail(email: string): Promise<void>;
-  isExistUserRealName(realname: string): Promise<void>;
-  isNotExistUserNickName(nickname: string): Promise<void>;
-  isExistUserPhoneNumber(phonenumber: string): Promise<void>;
-  isNotExistUserPhoneNumber(phonenumber: string): Promise<void>;
-}
