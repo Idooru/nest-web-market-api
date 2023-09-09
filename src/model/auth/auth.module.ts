253101bd559faaf7c28c0cf908db61ae927e4530
Idooru <shere1765@gmail.com>
2023-09-09 23:33:54 +0900

    [제거] 불필요한 클래스

--- src/model/auth/auth.module.ts
+++ src/model/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { UserProfileEntity } from "../user/entities/user-profile.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { forwardRef, Module } from "@nestjs/common";
-import { AuthGeneralService } from "./services/auth-general.service";
 import { JwtModule } from "@nestjs/jwt";
 import { UserAuthEntity } from "../user/entities/user-auth.entity";
 import { ConfigService } from "@nestjs/config";
 import { LibraryModule } from "src/common/lib/library.module";
 import { SecurityLibrary } from "src/common/lib/config/security.library";
 import { UserModule } from "../user/user.module";
-import { AuthExistService } from "./services/auth-exist.service";
 import { AdminUserEntity } from "../user/entities/admin-user.entity";
 import { ClientUserEntity } from "../user/entities/client-user.entity";
 import { UserEntity } from "../user/entities/user.entity";
@@ -31,7 +29,5 @@ import { UserEntity } from "../user/entities/user.entity";
     ),
     LibraryModule,
   ],
-  providers: [AuthGeneralService, AuthExistService],
-  exports: [AuthGeneralService],
 })
 export class AuthModule {}
