65de9bf00c2a05256d26d208a51c72742a3daab5
Idooru <shere1765@gmail.com>
2023-09-17 20:22:30 +0900

    [생성] User Validator 클래스

---
+++ src/model/user/logic/user.validator.ts
@@ -0,0 +1,25 @@
+import { UserSearcher } from "./user.searcher";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { UserValidateDto } from "../dtos/user-validate.dto";
+
+@Injectable()
+export class UserValidator {
+  async validate(
+    userSearcher: UserSearcher,
+    userValidateDto: UserValidateDto,
+  ): Promise<void> {
+    const { email, nickname, phonenumber } = userValidateDto;
+
+    const validResult = await Promise.allSettled([
+      userSearcher.isInvalidUserEmail(email),
+      userSearcher.isInvalidNickName(nickname),
+      userSearcher.isInvalidUserPhoneNumber(phonenumber),
+    ]);
+
+    const errors = validResult.filter((item) => item.status === "rejected");
+
+    if (errors.length) {
+      throw new NotFoundException(errors);
+    }
+  }
+}
