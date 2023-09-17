ee3f0729e1737a6c52b504800c5c306a565f9879
Idooru <shere1765@gmail.com>
2023-09-17 22:23:57 +0900

    [생성] Product Init 클래스

---
+++ src/model/product/logic/transaction/product.init.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { DataSource, QueryRunner } from "typeorm";
+import {
+  ProductRepositoryPayload,
+  ProductRepositoryVO,
+} from "../product-repository.vo";
+import { ProductEntity } from "../../entities/product.entity";
+import { StarRateEntity } from "../../../review/entities/star-rate.entity";
+import { ProductImageEntity } from "../../../media/entities/product-image.entity";
+
+@Injectable()
+export class ProductInit {
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly productRepositoryVO: ProductRepositoryVO,
+  ) {}
+
+  async init(): Promise<QueryRunner> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const repositoryPayload: ProductRepositoryPayload = {
+      productRepository: queryRunner.manager.getRepository(ProductEntity),
+      starRateRepository: queryRunner.manager.getRepository(StarRateEntity),
+      productImageRepository:
+        queryRunner.manager.getRepository(ProductImageEntity),
+    };
+
+    this.productRepositoryVO.setRepositoryPayload(repositoryPayload);
+
+    return queryRunner;
+  }
+}
