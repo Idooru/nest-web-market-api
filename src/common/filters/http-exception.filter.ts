10f5cd06117f7be1fe9b25a99f9ed79f673962b0
idooru <shere1765@gmail.com>
2023-09-20 22:10:53 +0900

    [수정] exception message가 배열인지 문자열인지 구분하여 json 응답 객체를 생성함

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -6,21 +6,47 @@ import {
 } from "@nestjs/common";
 import { Response } from "express";
 
+type ExceptionPayload = { status: string; reason: { message: string } };
+
+type ExceptionResponse = {
+  statusCode: number;
+  message: string | Array<ExceptionPayload>;
+};
+
+type JsonResponse = {
+  success: boolean;
+  error: string;
+  statusCode: number;
+  timestamp: Date;
+  reason?: string | string[];
+};
+
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private jsonResponse: JsonResponse;
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const res = host.switchToHttp().getResponse<Response>();
+    const exceptionResponse = exception.getResponse() as ExceptionResponse;
+
+    this.jsonResponse = {
+      success: false,
+      error: exception.name,
+      statusCode: exception.getStatus(),
+      timestamp: new Date(),
+    };
+
+    if (Array.isArray(exceptionResponse.message)) {
+      this.jsonResponse.reason = exceptionResponse.message.map(
+        (err) => err.reason.message,
+      );
+    } else {
+      this.jsonResponse.reason = exception.message;
+    }
 
     return res
       .status(exception.getStatus())
       .setHeader("X-Powered-By", "")
-      .json({
-        success: false,
-        error: exception.name,
-        statusCode: exception.getStatus(),
-        timestamp: new Date().toString(),
-        reason: exception.message,
-        info: "서버 로그를 확인하세요.",
-      });
+      .json(this.jsonResponse);
   }
 }
