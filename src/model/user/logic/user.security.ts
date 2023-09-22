7f40512bed2afb70efae98f015713c208f8edcda
idooru <shere1765@gmail.com>
2023-09-23 02:12:38 +0900

    [추가] 이메일로 사용자를 찾을 때 값이 없다면 400에러를 던짐

--- src/model/user/logic/user.security.ts
+++ src/model/user/logic/user.security.ts
@@ -68,6 +68,13 @@ export class UserSecurity {
     const { email, password } = loginUserDto;
 
     const user = await this.userSearcher.findUserWithEmail(email);
+
+    if (!user) {
+      const message = "아이디 혹은 비밀번호가 일치하지 않습니다.";
+      loggerFactory("Authenticate").error(message);
+      throw new BadRequestException(message);
+    }
+
     const isValidPw = await bcrypt
       .compare(password, user.Auth.password)
       .catch((err: Error) => {
