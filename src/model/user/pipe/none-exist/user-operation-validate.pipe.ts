6975f7507703e924b4104bc20cc2f78c17ae0b3e
idooru <shere1765@gmail.com>
2023-09-23 01:58:31 +0900

    [생성] User Operation Validate Pipe 클래스

---
+++ src/model/user/pipe/none-exist/user-operation-validate.pipe.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
+import { UserValidator } from "../../logic/user.validator";
+
+type UserBody = {
+  email?: string;
+  nickname?: string;
+  phonenumber?: string;
+};
+
+@Injectable()
+export class UserOperationValidatePipe<T extends UserBody>
+  implements PipeTransform
+{
+  constructor(private readonly userValidator: UserValidator) {}
+
+  public async transform(body: T): Promise<T> {
+    const { email, nickname, phonenumber } = body;
+
+    const validResult = await Promise.allSettled([
+      this.userValidator.isNoneExistEmail(email),
+      this.userValidator.isNoneExistNickname(nickname),
+      this.userValidator.isNoneExistPhonenumber(phonenumber),
+    ]);
+
+    const errors = validResult.filter((item) => item.status === "rejected");
+
+    if (errors.length) {
+      throw new BadRequestException(errors);
+    }
+
+    return body;
+  }
+}
