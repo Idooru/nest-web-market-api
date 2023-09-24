9b7622234557134e86b2342f10115065d7ac95f1
idooru <shere1765@gmail.com>
2023-09-24 16:28:28 +0900

    [생성] Inquiry Response Video Validate Pipe 클래스

---
+++ src/model/media/pipe/exist/inquiry-response-video-validate.pipe.ts
@@ -0,0 +1,22 @@
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { MediaValidator } from "../../logic/media.validator";
+import { MediaLoggerLibrary } from "../../../../common/lib/logger/media-logger.library";
+
+@Injectable()
+export class InquiryResponseVideoValidatePipe implements PipeTransform {
+  constructor(
+    private readonly mediaValidator: MediaValidator,
+    private readonly mediaLoggerLibrary: MediaLoggerLibrary,
+  ) {}
+
+  public transform(
+    inquiryResponseImage: Express.Multer.File[],
+  ): Express.Multer.File[] {
+    const mediaInfo = "문의 응답 비디오";
+
+    this.mediaValidator.validate(mediaInfo, inquiryResponseImage);
+    this.mediaLoggerLibrary.log(mediaInfo, inquiryResponseImage);
+
+    return inquiryResponseImage;
+  }
+}
