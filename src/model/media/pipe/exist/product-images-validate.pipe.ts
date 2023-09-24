59a4796f1f38e6e4ed85452a6d50fec104cc0a92
idooru <shere1765@gmail.com>
2023-09-24 16:28:57 +0900

    [생성] Product Images Validate Pipe 클래스

---
+++ src/model/media/pipe/exist/product-images-validate.pipe.ts
@@ -0,0 +1,22 @@
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { MediaLoggerLibrary } from "../../../../common/lib/logger/media-logger.library";
+import { MediaValidator } from "../../logic/media.validator";
+
+@Injectable()
+export class ProductImagesValidatePipe implements PipeTransform {
+  constructor(
+    private readonly mediaValidator: MediaValidator,
+    private readonly mediaLoggerLibrary: MediaLoggerLibrary,
+  ) {}
+
+  public transform(
+    productImages: Express.Multer.File[],
+  ): Express.Multer.File[] {
+    const mediaInfo = "상품 이미지";
+
+    this.mediaValidator.validate(mediaInfo, productImages);
+    this.mediaLoggerLibrary.log(mediaInfo, productImages);
+
+    return productImages;
+  }
+}
