f54a1049e4a6596d07401316b0fb91714c042628
idooru <shere1765@gmail.com>
2023-09-26 22:42:51 +0900

    [생성] Review Image Validate Pipe 클래스

---
+++ src/model/media/pipe/exist/review-image-validate.pipe.ts
@@ -0,0 +1,20 @@
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { MediaValidator } from "../../logic/media.validator";
+import { MediaLoggerLibrary } from "../../../../common/lib/logger/media-logger.library";
+
+@Injectable()
+export class ReviewImageValidatePipe implements PipeTransform {
+  constructor(
+    private readonly mediaValidator: MediaValidator,
+    private readonly mediaLoggerLibrary: MediaLoggerLibrary,
+  ) {}
+
+  public transform(reviewImage: Express.Multer.File[]): Express.Multer.File[] {
+    const mediaInfo = "리뷰 이미지";
+
+    this.mediaValidator.validate(mediaInfo, reviewImage);
+    this.mediaLoggerLibrary.log(mediaInfo, reviewImage);
+
+    return reviewImage;
+  }
+}
