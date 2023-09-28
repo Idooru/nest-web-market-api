4c21aa953a9b2eae6251cf94c926171090406fcd
idooru <shere1765@gmail.com>
2023-09-28 19:37:19 +0900

    [수정] 매개변수 타입

--- src/model/product/pipe/none-exist/product-name-validate.pipe.ts
+++ src/model/product/pipe/none-exist/product-name-validate.pipe.ts
@@ -1,13 +1,14 @@
 import { Injectable, PipeTransform } from "@nestjs/common";
 import { ProductValidator } from "../../logic/product.validator";
+import { ProductBodyDto } from "../../dto/product-body.dto";
 
 @Injectable()
 export class ProductNameValidatePipe implements PipeTransform {
   constructor(private readonly productValidator: ProductValidator) {}
 
-  public async transform(name: string): Promise<string> {
-    await this.productValidator.isNoneExistName(name);
+  public async transform(value: ProductBodyDto): Promise<ProductBodyDto> {
+    await this.productValidator.isNoneExistName(value.name);
 
-    return name;
+    return value;
   }
 }
