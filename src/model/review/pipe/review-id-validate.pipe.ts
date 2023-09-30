a4f69be4651019b78159a94a45e66a4aba09e5f3
idooru <shere1765@gmail.com>
2023-09-30 23:07:58 +0900

    [생성] Review Id Validate Pipe 클래스

---
+++ src/model/review/pipe/review-id-validate.pipe.ts
@@ -0,0 +1,13 @@
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { ReviewValidator } from "../logic/review.validator";
+
+@Injectable()
+export class ReviewIdValidatePipe implements PipeTransform {
+  constructor(private readonly reviewValidator: ReviewValidator) {}
+
+  public async transform(id: string): Promise<string> {
+    await this.reviewValidator.isExistId(id);
+
+    return id;
+  }
+}
