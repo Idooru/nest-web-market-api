fe13d693eeb39d9eaa076e3e0b8f40125d6a7830
idooru <shere1765@gmail.com>
2023-09-30 23:07:16 +0900

    [생성] Review Validate Repository 클래스

---
+++ src/model/review/repositories/review-validate.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ReviewEntity } from "../entities/review.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class ReviewValidateRepository {
+  constructor(
+    @InjectRepository(ReviewEntity)
+    private readonly reviewRepository: Repository<ReviewEntity>,
+  ) {}
+
+  async isExistId(id: string): Promise<boolean> {
+    return await this.reviewRepository.exist({ where: { id } });
+  }
+}
