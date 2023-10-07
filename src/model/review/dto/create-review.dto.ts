e861e724e9b0299414ffcdec160af9219aa8ac7e
idooru <shere1765@gmail.com>
2023-10-07 22:45:59 +0900

    [수정] 미디어 파일 여부에 대한 Dto 분기

--- src/model/review/dto/create-review.dto.ts
+++ src/model/review/dto/create-review.dto.ts
@@ -3,16 +3,29 @@ import { ClientUserEntity } from "src/model/user/entities/client-user.entity";
 import { ReviewBodyDto } from "./review-body.dto";
 import { MediaCookieDto } from "../../media/dto/media-cookie.dto";
 
-export class CreateReviewDto {
-  reviewRequestDto: ReviewBodyDto;
-  userId?: string;
+class ReviewBasicDto {
+  reviewBodyDto: ReviewBodyDto;
+  userId: string;
   productId: string;
-  reviewImgCookies?: MediaCookieDto[];
-  reviewVdoCookies?: MediaCookieDto[];
 }
 
-export class CreateReviewDao {
-  reviewRequestDto: ReviewBodyDto;
+export class CreateReviewAllMediaDto extends ReviewBasicDto {
+  reviewImgCookies: MediaCookieDto[];
+  reviewVdoCookies: MediaCookieDto[];
+}
+
+export class CreateReviewImageDto extends ReviewBasicDto {
+  reviewImgCookies: MediaCookieDto[];
+}
+
+export class CreateReviewVideoDto extends ReviewBasicDto {
+  reviewVdoCookies: MediaCookieDto[];
+}
+
+export class CreateReviewNoMediaDto extends ReviewBasicDto {}
+
+export class CreateReviewDto {
+  reviewBodyDto: ReviewBodyDto;
   client: ClientUserEntity;
   product: ProductEntity;
 }
--- src/model/review/dto/modify-review.dto.ts
+++ src/model/review/dto/modify-review.dto.ts
@@ -1,9 +1,30 @@
-import { JwtAccessTokenPayload } from "src/model/auth/jwt/jwt-access-token-payload.interface";
 import { ReviewEntity } from "../entities/review.entity";
 import { ReviewBodyDto } from "./review-body.dto";
+import { MediaCookieDto } from "../../media/dto/media-cookie.dto";
+
+class ReviewBasicDto {
+  reviewBodyDto: ReviewBodyDto;
+  userId: string;
+  productId: string;
+  reviewId: string;
+}
+
+export class ModifyReviewAllMediaDto extends ReviewBasicDto {
+  reviewImgCookies: MediaCookieDto[];
+  reviewVdoCookies: MediaCookieDto[];
+}
+
+export class ModifyReviewImageDto extends ReviewBasicDto {
+  reviewImgCookies: MediaCookieDto[];
+}
+
+export class ModifyReviewVideoDto extends ReviewBasicDto {
+  reviewVdoCookies: MediaCookieDto[];
+}
+
+export class ModifyReviewNoMediaDto extends ReviewBasicDto {}
 
 export class ModifyReviewDto {
-  reviewRequestDto: ReviewBodyDto;
-  jwtPayload: JwtAccessTokenPayload;
-  beforeReview: ReviewEntity;
+  reviewBodyDto: ReviewBodyDto;
+  review: ReviewEntity;
 }
