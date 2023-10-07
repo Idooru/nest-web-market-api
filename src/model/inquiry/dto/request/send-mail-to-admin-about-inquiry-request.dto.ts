49c39219757087b0a13092fc7c4597d093c3c3ed
idooru <shere1765@gmail.com>
2023-10-07 23:07:36 +0900

    [추가] client 필드

--- src/model/inquiry/dto/request/send-mail-to-admin-about-inquiry-request.dto.ts
+++ src/model/inquiry/dto/request/send-mail-to-admin-about-inquiry-request.dto.ts
@@ -1,7 +1,9 @@
 import { ProductEntity } from "src/model/product/entities/product.entity";
 import { InquiryRequestEntity } from "../../entities/inquiry-request.entity";
+import { ClientUserEntity } from "../../../user/entities/client-user.entity";
 
 export class SendMailToAdminAboutInquiryRequestDto {
   product: ProductEntity;
   inquiryRequest: InquiryRequestEntity;
+  client: ClientUserEntity;
 }
