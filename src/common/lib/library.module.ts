58059b2a54a6014456fd4c7079677df7530db313
idooru <shere1765@gmail.com>
2023-10-08 14:19:43 +0900

    [추가] 필요한 의존성

--- src/common/lib/library.module.ts
+++ src/common/lib/library.module.ts
@@ -1,17 +1,17 @@
 import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { DotenvConfigurationModule } from "../config/dotenv.config";
-import { MeidaLoggerLibrary } from "./logger/media-logger.library";
+import { MediaLoggerLibrary } from "./logger/media-logger.library";
 import { SecurityLibrary } from "./config/security.library";
 import { TimeLoggerLibrary } from "./logger/time-logger.library";
 import { EmailSenderLibrary } from "./email/email-sender.library";
 import { MailerConfigurationModule } from "../config/mailer.config";
 import { TypeOrmErrorHandlingBuilder } from "./error-handler/typeorm-error-handling.builder";
-import { LibraryErrorHandlingBuilder } from "./error-handler/library-error-handling.builder";
 import { JwtErrorHandlingBuilder } from "./error-handler/jwt-error-handling.builder";
 import { HttpExceptionHandlingBuilder } from "./error-handler/http-exception-handling.builder";
 import { ErrorLoggerLibrary } from "./logger/error-logger.library";
 import { ValidateLibrary } from "./util/validate.library";
+import { TransactionErrorHandler } from "./error-handler/transaction-error.handler";
 
 @Module({
   imports: [DotenvConfigurationModule, MailerConfigurationModule],
@@ -21,24 +21,24 @@ import { ValidateLibrary } from "./util/validate.library";
     EmailSenderLibrary,
     HttpExceptionHandlingBuilder,
     TypeOrmErrorHandlingBuilder,
-    LibraryErrorHandlingBuilder,
     JwtErrorHandlingBuilder,
     TimeLoggerLibrary,
-    MeidaLoggerLibrary,
+    MediaLoggerLibrary,
     ErrorLoggerLibrary,
     ValidateLibrary,
+    TransactionErrorHandler,
   ],
   exports: [
     SecurityLibrary,
     EmailSenderLibrary,
     HttpExceptionHandlingBuilder,
     TypeOrmErrorHandlingBuilder,
-    LibraryErrorHandlingBuilder,
     JwtErrorHandlingBuilder,
     TimeLoggerLibrary,
-    MeidaLoggerLibrary,
+    MediaLoggerLibrary,
     ErrorLoggerLibrary,
     ValidateLibrary,
+    TransactionErrorHandler,
   ],
 })
 export class LibraryModule {}
