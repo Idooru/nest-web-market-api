ca121158bbf126a22aa22b9c59ff7ff416a3f4bc
idooru <shere1765@gmail.com>
2023-10-09 12:38:11 +0900

    [생성] catch-callback-factory.library.ts

---
+++ src/common/lib/util/catch-callback-factory.library.ts
@@ -0,0 +1,108 @@
+import { InternalServerErrorException } from "@nestjs/common";
+import { loggerFactory } from "../../functions/logger.factory";
+import { LibraryException } from "../../errors/library.exception";
+
+export class CatchCallbackFactoryLibrary {
+  private throwLibraryException(errors: {
+    err: Error;
+    errorCase: string;
+    libraryName: string;
+    hasTransaction: boolean;
+  }): never {
+    const { err, errorCase, libraryName, hasTransaction } = errors;
+    loggerFactory(errorCase).error(err);
+
+    if (hasTransaction) {
+      throw new InternalServerErrorException({
+        errorCase,
+        libraryName,
+        message: err.message,
+        type: "library",
+      });
+    }
+
+    throw new LibraryException({
+      response: {
+        errorCase,
+        libraryName,
+        message: err.message,
+      },
+    });
+  }
+
+  public getCatchHashPasswordFunc(
+    hasTransaction: boolean,
+  ): (err: Error) => never {
+    return (err: Error) => {
+      const errorCase = "HashPassword";
+      const libraryName = "bcrypt";
+
+      this.throwLibraryException({
+        err,
+        errorCase,
+        libraryName,
+        hasTransaction,
+      });
+    };
+  }
+
+  public getCatchComparePasswordFunc(): (err: Error) => never {
+    return (err: Error) => {
+      const errorCase = "ComparePassword";
+      const libraryName = "bcrypt";
+      const hasTransaction = false;
+
+      this.throwLibraryException({
+        err,
+        errorCase,
+        libraryName,
+        hasTransaction,
+      });
+    };
+  }
+
+  public getCatchJwtTokenSignFunc(): (err: Error) => never {
+    return (err: Error) => {
+      const errorCase = "JwtTokenSign";
+      const libraryName = "JwtService";
+      const hasTransaction = false;
+
+      this.throwLibraryException({
+        err,
+        errorCase,
+        libraryName,
+        hasTransaction,
+      });
+    };
+  }
+
+  public getCatchJwtTokenVerifyFunc(): (err: Error) => never {
+    return (err: Error) => {
+      const errorCase = "JwtTokenVerify";
+      const libraryName = "JwtService";
+      const hasTransaction = false;
+
+      this.throwLibraryException({
+        err,
+        errorCase,
+        libraryName,
+        hasTransaction,
+      });
+    };
+  }
+
+  public getCatchSendMailFunc(): (err: Error) => never {
+    return (err: Error) => {
+      const errorCase = "SendMail";
+      const libraryName = "MailerService";
+      const hasTransaction = false;
+
+      this.throwLibraryException({
+        err,
+        errorCase,
+        libraryName,
+        hasTransaction,
+      });
+    };
+  }
+}
