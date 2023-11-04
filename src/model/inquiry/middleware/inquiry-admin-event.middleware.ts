e2f49bf60f230b164b15282b2aa4b90c5fa0ae7c
idooru <shere1765@gmail.com>
2023-11-04 18:35:31 +0900

    [생성] inquiry-admin-event.middleware.ts

---
+++ src/model/inquiry/middleware/inquiry-admin-event.middleware.ts
@@ -0,0 +1,30 @@
+import { Inject, Injectable, NestMiddleware } from "@nestjs/common";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { Request, Response, NextFunction } from "express";
+import { SendMailToClientAboutInquiryResponseDto } from "../dto/response/send-mail-to-client-about-inquiry-response.dto";
+import { eventConfigs } from "../../../common/config/event-configs";
+
+@Injectable()
+export class InquiryAdminEventMiddleware implements NestMiddleware {
+  constructor(
+    @Inject("InquiryEventMap")
+    private readonly inquiryEventMap: Map<string, any>,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
+
+  use(req: Request, res: Response, next: NextFunction): void {
+    res.on("finish", () => {
+      const sendMailToClientAboutInquiryResponseDto: SendMailToClientAboutInquiryResponseDto =
+        this.inquiryEventMap.get("admin");
+
+      if (!sendMailToClientAboutInquiryResponseDto) return;
+
+      this.eventEmitter.emit(
+        eventConfigs.sendMailInquiryResponse,
+        sendMailToClientAboutInquiryResponseDto,
+      );
+    });
+
+    next();
+  }
+}
