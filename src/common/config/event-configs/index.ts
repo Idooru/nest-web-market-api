349c150b535528ce6abc3e8557cfa45037d0f7c1
idooru <shere1765@gmail.com>
2023-11-04 17:24:31 +0900

    [수정] index.ts

---
+++ src/common/config/event-configs/index.ts
@@ -0,0 +1,5 @@
+export const eventConfigs = {
+  sendMailInquiryRequest: "send-mail-inquiry-request",
+  sendMailInquiryResponse: "send-mail-inquiry-response",
+  sendMailRegister: "send-mail-register",
+};
