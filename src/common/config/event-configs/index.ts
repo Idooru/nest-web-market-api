7992c1ce88b28351c3a0a3ce303d6072bdc40a3f
idooru <shere1765@gmail.com>
2023-11-07 09:47:11 +0900

    [생성] media-delete-review-image.middleware.ts

--- src/common/config/event-configs/index.ts
+++ src/common/config/event-configs/index.ts
@@ -2,6 +2,7 @@ export const eventConfigs = {
   sendMailInquiryRequest: "send-mail-inquiry-request",
   sendMailInquiryResponse: "send-mail-inquiry-response",
   sendMailRegister: "send-mail-register",
+  deleteMediaFile: "delete-media-file",
 };
 
 export const eventMap = new Map<string, any>();
