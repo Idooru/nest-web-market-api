306d8f1b3df73f9bceee0375c0b1bc03bb4a74fc
idooru <shere1765@gmail.com>
2023-11-11 21:45:12 +0900

    [제거] 쿠키 검증 함수 호출

--- src/common/decorators/media-cookies-parser.decorator.ts
+++ src/common/decorators/media-cookies-parser.decorator.ts
@@ -1,13 +1,10 @@
 import { createParamDecorator, ArgumentsHost } from "@nestjs/common";
 import { Request } from "express";
 import { MediaCookie, MediaCookies } from "../types/media-cookies";
-import { cookiesFilter } from "./cookies-filter";
 
 export const MediaCookiesParser = createParamDecorator(
   (data: string, context: ArgumentsHost): Array<MediaCookie> => {
     const req = context.switchToHttp().getRequest() as Request;
-
-    cookiesFilter(req, data);
 
     return Object.entries(req.signedCookies)
       .filter((mediaCookie: MediaCookies) => mediaCookie[0].includes(data))
