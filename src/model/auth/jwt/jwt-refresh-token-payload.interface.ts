1b0d8430890605c76bc53578d75a9fcc3e326ba6
idooru <shere1765@gmail.com>
2023-12-02 23:32:18 +0900

    [제거] 불필요한 필드

--- src/model/auth/jwt/jwt-refresh-token-payload.interface.ts
+++ src/model/auth/jwt/jwt-refresh-token-payload.interface.ts
@@ -2,5 +2,4 @@ import { JwtAccessTokenPayload } from "./jwt-access-token-payload.interface";
 
 export interface JwtRefreshTokenPayload extends JwtAccessTokenPayload {
   isRefreshToken: boolean;
-  randomToken: string;
 }
