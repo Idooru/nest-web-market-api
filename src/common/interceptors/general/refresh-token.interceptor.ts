fc452068350fad6b02e11e872c921c056628a0e9
idooru <shere1765@gmail.com>
2023-12-02 22:44:16 +0900

    [생성] refresh-token.interceptor.ts

---
+++ src/common/interceptors/general/refresh-token.interceptor.ts
@@ -0,0 +1,39 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from "@nestjs/common";
+import { map, Observable } from "rxjs";
+import { Request, Response } from "express";
+import { TimeLoggerLibrary } from "../../lib/logger/time-logger.library";
+import { SecurityLibrary } from "../../lib/security/security.library";
+import { RefreshTokenInterface } from "../interface/refresh-token.interface";
+
+@Injectable()
+export class RefreshTokenInterceptor implements NestInterceptor {
+  constructor(
+    private readonly timeLoggerLibrary: TimeLoggerLibrary,
+    private readonly securityLibrary: SecurityLibrary,
+  ) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest<Request>();
+    const res = context.switchToHttp().getResponse<Response>();
+    const cookieOption = this.securityLibrary.cookieOption;
+
+    this.timeLoggerLibrary.receiveRequest(req);
+
+    return next.handle().pipe(
+      map((data: RefreshTokenInterface) => {
+        const { statusCode, message, cookieKey, cookieValue } = data;
+        this.timeLoggerLibrary.sendResponse(req);
+
+        res.cookie(cookieKey, cookieValue, cookieOption);
+
+        res.status(data.statusCode).setHeader("X-Powered-By", "");
+        return { success: true, ...{ statusCode, message } };
+      }),
+    );
+  }
+}
