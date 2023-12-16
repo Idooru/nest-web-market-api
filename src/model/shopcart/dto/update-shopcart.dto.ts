06c674758fb9e629f45a530875b6db8306516897
idooru <shere1765@gmail.com>
2023-12-16 21:24:07 +0900

    [제거] 불필요한 클래스

--- src/model/shopcart/dto/update-shopcart.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateShopcartDto } from "./create-shopcart.dto";
-
-export class UpdateShopcartDto extends PartialType(CreateShopcartDto) {}
--- src/model/shopcart/shopcart.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ShopcartService } from './shopcart.service';
-import { ShopcartController } from './shopcart.controller';
-
-@Module({
-  controllers: [ShopcartController],
-  providers: [ShopcartService]
-})
-export class ShopcartModule {}
