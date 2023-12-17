e26d75faadb382e5b61d5eee9b4d6b53a926256c
idooru <shere1765@gmail.com>
2023-12-17 23:34:42 +0900

    [생성] cart.validator.ts

---
+++ src/model/cart/logic/cart.validator.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { ValidateLibrary } from "../../../common/lib/util/validate.library";
+import { CartValidateRepository } from "../repositories/cart-validate.repository";
+
+@Injectable()
+export class CartValidator {
+  constructor(
+    private readonly cartValidateRepository: CartValidateRepository,
+    private readonly validateLibrary: ValidateLibrary,
+  ) {}
+
+  public async isExistId(id: string): Promise<void> {
+    const result = await this.cartValidateRepository.isExistId(id);
+    this.validateLibrary.isExistData(result, "cart id", id);
+  }
+}
