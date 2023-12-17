1e1ff6626bbbc488892a29d39a4208f9bdd0b92a
idooru <shere1765@gmail.com>
2023-12-17 22:27:13 +0900

    [추가] 장바구니 컬럼

--- src/model/cart/entities/cart.entity.ts
+++ src/model/cart/entities/cart.entity.ts
@@ -1,5 +1,33 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import { CommonEntity } from "../../../common/entities/common.entity";
+import { ProductEntity } from "../../product/entities/product.entity";
+import { ClientUserEntity } from "../../user/entities/client-user.entity";
+import { IsEnum, IsNotEmpty, IsPositive, Max } from "class-validator";
 
 @Entity({ name: "carts", synchronize: true })
-export class CartEntity extends CommonEntity {}
+export class CartEntity extends CommonEntity {
+  @IsEnum(["default", "speed", "safe"])
+  @IsNotEmpty()
+  @Column({ type: "enum", enum: ["default", "speed", "safe"] })
+  deliveryOption: "default" | "speed" | "safe";
+
+  @IsPositive()
+  @IsNotEmpty()
+  @Max(50)
+  @Column({ type: "int", unsigned: true })
+  quantity: number;
+
+  @IsPositive()
+  @IsNotEmpty()
+  @Column({ type: "int", unsigned: true, nullable: false })
+  totalPrice: number;
+
+  @IsPositive()
+  @ManyToOne(() => ClientUserEntity, (clientUser) => clientUser.cart)
+  @JoinColumn({ name: "clientId" })
+  clientUser: ClientUserEntity;
+
+  @ManyToOne(() => ProductEntity, (product) => product.cart)
+  @JoinColumn({ name: "productId" })
+  product: ProductEntity;
+}
