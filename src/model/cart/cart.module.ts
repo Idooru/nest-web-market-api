d6075a96b9ce56e0cf8d0e85c2cc172625c01868
idooru <shere1765@gmail.com>
2023-12-17 23:23:16 +0900

    [추가] 장바구니 모듈 구성

--- src/model/cart/cart.module.ts
+++ src/model/cart/cart.module.ts
@@ -1,7 +1,30 @@
 import { Module } from "@nestjs/common";
+import { CartV1ClientControllerController } from "./controllers/v1/cart-v1-client.controller.controller";
+import { CartUpdateService } from "./services/cart-update.service";
+import { CartSearchRepository } from "./repositories/cart-search.repository";
+import { CartUpdateRepository } from "./repositories/cart-update.repository";
+import { CartSearcher } from "./logic/cart.searcher";
+import { LibraryModule } from "../../common/lib/library.module";
+import { ProductModule } from "../product/product.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CartEntity } from "./entities/cart.entity";
+import { UserModule } from "../user/user.module";
+import { cartSelectProperty } from "../../common/config/repository-select-configs/cart.select";
 
 @Module({
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forFeature([CartEntity]),
+    LibraryModule,
+    UserModule,
+    ProductModule,
+  ],
+  controllers: [CartV1ClientControllerController],
+  providers: [
+    { provide: "CartsSelectProperty", useValue: cartSelectProperty },
+    CartUpdateService,
+    CartSearchRepository,
+    CartUpdateRepository,
+    CartSearcher,
+  ],
 })
 export class CartModule {}
