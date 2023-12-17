1fe19874a10675090e4e03d9d8d01781c296b1e1
idooru <shere1765@gmail.com>
2023-12-17 22:27:37 +0900

    [추가] 배송 옵션 필드

--- src/model/cart/dto/cart-body.dto.ts
+++ src/model/cart/dto/cart-body.dto.ts
@@ -1,7 +1,18 @@
 import { ApiProperty, PickType } from "@nestjs/swagger";
 import { CartEntity } from "../entities/cart.entity";
 
-export class CartBodyDto extends PickType(CartEntity, ["quantity"] as const) {
+export class CartBodyDto extends PickType(CartEntity, [
+  "deliveryOption",
+  "quantity",
+] as const) {
+  @ApiProperty({
+    description: "배송 옵션",
+    example: "default",
+    required: true,
+    uniqueItems: false,
+  })
+  deliveryOption: "default" | "speed" | "safe";
+
   @ApiProperty({
     description: "장바구니 상품 수량",
     example: 5,
