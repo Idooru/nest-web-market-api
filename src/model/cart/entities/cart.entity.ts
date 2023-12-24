abe0d1a9d69c731ca4c6665fe5fc42b91794291f
idooru <shere1765@gmail.com>
2023-12-24 23:01:22 +0900

    [제거] 불필요한 필드

--- src/model/cart/entities/cart.entity.ts
+++ src/model/cart/entities/cart.entity.ts
@@ -2,15 +2,10 @@ import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import { CommonEntity } from "../../../common/entities/common.entity";
 import { ProductEntity } from "../../product/entities/product.entity";
 import { ClientUserEntity } from "../../user/entities/client-user.entity";
-import { IsEnum, IsNotEmpty, IsPositive, Max } from "class-validator";
+import { IsNotEmpty, IsPositive, Max } from "class-validator";
 
 @Entity({ name: "carts", synchronize: true })
 export class CartEntity extends CommonEntity {
-  @IsEnum(["default", "speed", "safe"])
-  @IsNotEmpty()
-  @Column({ type: "enum", enum: ["default", "speed", "safe"] })
-  deliveryOption: "default" | "speed" | "safe";
-
   @IsPositive()
   @IsNotEmpty()
   @Max(50)
@@ -22,7 +17,6 @@ export class CartEntity extends CommonEntity {
   @Column({ type: "int", unsigned: true, nullable: false })
   totalPrice: number;
 
-  @IsPositive()
   @ManyToOne(() => ClientUserEntity, (clientUser) => clientUser.cart)
   @JoinColumn({ name: "clientId" })
   clientUser: ClientUserEntity;
