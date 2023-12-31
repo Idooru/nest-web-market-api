53472170f8452afd5e33bdb15206b69341db085d
idooru <shere1765@gmail.com>
2023-12-31 23:30:05 +0900

    [생성] order-v1-client.contoller.ts

---
+++ src/model/order/controllers/v1/order-v1-client.contoller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { JsonGeneralInterceptor } from "../../../../common/interceptors/general/json-general.interceptor";
+import { JsonGeneralInterface } from "../../../../common/interceptors/interface/json-general-interface";
+import { GetJWT } from "../../../../common/decorators/get.jwt.decorator";
+import { JwtAccessTokenPayload } from "../../../auth/jwt/jwt-access-token-payload.interface";
+import { IsClientGuard } from "../../../../common/guards/authenticate/is-client.guard";
+import { IsLoginGuard } from "../../../../common/guards/authenticate/is-login.guard";
+import { OrderBodyDto } from "../../dto/order-body.dto";
+import { OrderTransaction } from "../../logic/transaction/order.transaction";
+
+@ApiTags("v1 고객 Order API")
+@UseGuards(IsClientGuard)
+@UseGuards(IsLoginGuard)
+@Controller({ path: "/client/order", version: "1" })
+export class OrderV1ClientContoller {
+  constructor(private readonly orderTransaction: OrderTransaction) {}
+
+  @ApiOperation({})
+  @UseInterceptors(JsonGeneralInterceptor)
+  @Post("/")
+  public async createOrder(
+    @GetJWT() jwtPayload: JwtAccessTokenPayload,
+    @Body() orderBodyDto: OrderBodyDto,
+  ): Promise<JsonGeneralInterface<void>> {
+    await this.orderTransaction.createOrder({
+      clientId: jwtPayload.userId,
+      orderBodyDto,
+    });
+
+    return {
+      statusCode: 201,
+      message: "결제 주문이 완료되었습니다.",
+    };
+  }
+}
