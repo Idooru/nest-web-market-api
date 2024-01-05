84cfe244995496016eefea6cd017f794d9503de5
idooru <shere1765@gmail.com>
2024-01-06 00:26:26 +0900

    [생성] order-transaction.context.ts

---
+++ src/model/order/logic/transaction/order-transaction.context.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@nestjs/common";
+import { SearchCreateOrderDto } from "../../dto/search-create-order.dto";
+import { OrderUpdateService } from "../../services/order-update.service";
+
+@Injectable()
+export class OrderTransactionContext {
+  constructor(private readonly orderUpdateService: OrderUpdateService) {}
+
+  public createOrderContext(dto: SearchCreateOrderDto): () => Promise<void> {
+    const {
+      clientId,
+      orderBodyDto,
+      clientUser,
+      account,
+      totalPrice,
+      productQuantities,
+    } = dto;
+
+    return async () => {
+      await Promise.all([
+        this.orderUpdateService.deleteAllCarts(clientId),
+        this.orderUpdateService.decreaseProductQuantities(productQuantities),
+      ]);
+
+      const order = await this.orderUpdateService.createOrder({
+        orderBodyDto,
+        totalPrice,
+        clientUser,
+      });
+
+      await this.orderUpdateService.createPayments({
+        productQuantities,
+        clientUser,
+        order,
+      });
+
+      await this.orderUpdateService.withdrawClientBalance({
+        accountId: account.id,
+        balance: totalPrice,
+      });
+
+      await this.orderUpdateService.depositAdminBalance(productQuantities);
+    };
+  }
+}
