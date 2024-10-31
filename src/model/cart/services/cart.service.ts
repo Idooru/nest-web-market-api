b0112e85d302ef6aa7f8f9d09d3d5a86080f04cc
idooru <shere1765@gmail.com>
2024-10-31 09:45:14 +0900

    [수정] 공통 로직 분리

--- src/model/cart/services/cart.service.ts
+++ src/model/cart/services/cart.service.ts
@@ -7,6 +7,7 @@ import { CartBodyDto } from "../dto/cart-body.dto";
 import { ModifyCartDto } from "../dto/modify-cart.dto";
 import { loggerFactory } from "../../../common/functions/logger.factory";
 import { General } from "../../../common/decorators/general.decoration";
+import { ProductEntity } from "src/model/product/entities/product.entity";
 
 @Injectable()
 export class CartService {
@@ -17,9 +18,18 @@ export class CartService {
     private readonly productSearcher: ProductSearcher,
   ) {}
 
+  private validateProductAmount(product: ProductEntity, dto: CartBodyDto): void {
+    const { quantity, totalPrice } = dto;
+
+    if (product.price * quantity !== totalPrice) {
+      const message = `상품의 총 가격이 가격과 수량의 곱과 같지 않습니다.`;
+      loggerFactory("Not Same").error(message);
+      throw new BadRequestException(message);
+    }
+  }
+
   @General
   public async createCart(productId: string, clientUserId: string, dto: CartBodyDto): Promise<void> {
-    const { quantity, totalPrice } = dto;
     await this.cartSearcher.validateProduct(clientUserId, productId);
 
     const [clientUser, product] = await Promise.all([
@@ -27,11 +37,7 @@ export class CartService {
       this.productSearcher.findProductWithId(productId),
     ]);
 
-    if (product.price * quantity !== totalPrice) {
-      const message = `상품의 총 가격이 가격과 수량의 곱과 같지 않습니다.`;
-      loggerFactory("Not Same").error(message);
-      throw new BadRequestException(message);
-    }
+    this.validateProductAmount(product, dto);
 
     await this.cartUpdateRepository.createCart({
       product,
@@ -43,14 +49,9 @@ export class CartService {
   @General
   public async modifyCartWithId(dto: ModifyCartDto): Promise<void> {
     const { productId } = dto;
-    const { quantity, totalPrice } = dto.cartBodyDto;
     const product = await this.productSearcher.findProductWithId(productId);
 
-    if (product.price * quantity !== totalPrice) {
-      const message = `상품의 총 가격이 가격과 수량의 곱과 같지 않습니다.`;
-      loggerFactory("Not Same").error(message);
-      throw new BadRequestException(message);
-    }
+    this.validateProductAmount(product, dto.cartBodyDto);
 
     await this.cartUpdateRepository.modifyCartWithId(dto);
   }
