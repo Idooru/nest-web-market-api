459367b060ac4acdb40f261b3a0ccb7e357f492a
idooru <shere1765@gmail.com>
2025-03-19 16:57:41 +0900

    [생성] logout.swagger.ts

---
+++ src/model/user/docs/user-v1-controller/logout.swagger.ts
@@ -0,0 +1,29 @@
+import { applyDecorators } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { JwtAuthFailSwagger, JwtAuthHeaderSwagger } from "../../../auth/docs/jwt-auth.swagger";
+import { ServerErrorSwagger } from "../../../../common/docs/internal-server-error.swager";
+
+export const LogoutSwagger = () => {
+  return applyDecorators(
+    ApiOperation({
+      summary: "logout",
+      description: "로그아웃을 합니다. access token이 담긴 request header와 db에 저장된 refresh token을 제거합니다.",
+    }),
+    JwtAuthHeaderSwagger(),
+    ApiResponse({
+      status: 200,
+      description: "로그아웃 성공",
+      content: {
+        "application/json": {
+          example: {
+            success: true,
+            statusCode: 200,
+            message: "로그아웃을 완료하였습니다.",
+          },
+        },
+      },
+    }),
+    ServerErrorSwagger(),
+    JwtAuthFailSwagger(),
+  );
+};
