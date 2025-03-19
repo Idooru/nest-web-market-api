f29f659b5b4416f93835c70ee8944c11d188cffd
idooru <shere1765@gmail.com>
2025-03-19 17:01:11 +0900

    [생성] secession.swagger.ts

---
+++ src/model/user/docs/user-v1-controller/secession.swagger.ts
@@ -0,0 +1,26 @@
+import { applyDecorators } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { JwtAuthFailSwagger, JwtAuthHeaderSwagger } from "../../../auth/docs/jwt-auth.swagger";
+import { ServerErrorSwagger } from "../../../../common/docs/internal-server-error.swager";
+
+export const SecessionSwagger = () => {
+  return applyDecorators(
+    ApiOperation({ summary: "secession", description: "회원 탈퇴를 합니다." }),
+    JwtAuthHeaderSwagger(),
+    ApiResponse({
+      status: 200,
+      description: "회원 탈퇴 성공",
+      content: {
+        "application/json": {
+          example: {
+            success: true,
+            statusCode: 200,
+            message: "사용자 정보를 삭제하였습니다.",
+          },
+        },
+      },
+    }),
+    ServerErrorSwagger(),
+    JwtAuthFailSwagger(),
+  );
+};
