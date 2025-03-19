69a838702148dee64d175ec5c498bb1265eee34c
idooru <shere1765@gmail.com>
2025-03-19 17:02:10 +0900

    [생성] reset-password.swagger.ts

---
+++ src/model/user/docs/user-v1-controller/reset-password.swagger.ts
@@ -0,0 +1,29 @@
+import { applyDecorators } from "@nestjs/common";
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { ResetPasswordDto } from "../../dtos/reset-password.dto";
+import { ServerErrorSwagger } from "../../../../common/docs/internal-server-error.swager";
+
+export const ResetPasswordSwagger = () => {
+  return applyDecorators(
+    ApiOperation({
+      summary: "reset password",
+      description:
+        "비밀번호를 재설정합니다. 본인의 이메일과 변경할 비밀번호를 사용합니다. 해당 API는 비밀번호를 잃어버려 재설정하는 용도로 사용할 수 있도록 합니다",
+    }),
+    ApiBody({ type: ResetPasswordDto, required: true }),
+    ApiResponse({
+      status: 200,
+      description: "사용자 비밀번호 재설정 성공",
+      content: {
+        "application/json": {
+          example: {
+            success: true,
+            statusCode: 200,
+            message: "사용자 비밀번호를 재설정 하였습니다.",
+          },
+        },
+      },
+    }),
+    ServerErrorSwagger(),
+  );
+};
