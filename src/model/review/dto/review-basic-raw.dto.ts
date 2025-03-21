a748bcc26c82f1bdbbc0afd88feba067f018cb3c
idooru <shere1765@gmail.com>
2025-03-21 17:19:40 +0900

    [생성] review-basic-raw.dto.ts

---
+++ src/model/review/dto/review-basic-raw.dto.ts
@@ -0,0 +1,11 @@
+export class ReviewBasicRawDto {
+  reviewerId: string;
+  reviewId: string;
+  reviewTitle: string;
+  starRateScore: number;
+  countForModify: number;
+  productId: string;
+  productName: string;
+  productPrice: number;
+  productCategory: string;
+}
