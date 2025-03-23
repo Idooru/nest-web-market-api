3adc8abdcc118bc911ba97fade77b864df0e13f6
idooru <shere1765@gmail.com>
2025-03-23 10:55:17 +0900

    [생성] product-basic-raw.dto.ts

---
+++ src/model/product/dto/response/product-basic-raw.dto.ts
@@ -0,0 +1,10 @@
+export class ProductBasicRawDto {
+  productId: string;
+  productName: string;
+  productPrice: number;
+  productCategory: string;
+  productCreatedAt: string;
+  productImageUrls: string[];
+  averageScore: number;
+  reviewCount: number;
+}
