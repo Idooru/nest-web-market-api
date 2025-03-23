1d46e5d407655f25489111f04b93eca65d323011
idooru <shere1765@gmail.com>
2025-03-23 10:41:58 +0900

    [수정] 파일 경로

--- src/model/product/dto/search-modify-product.dto.ts
+++ src/model/product/dto/search-modify-product.dto.ts
@@ -1,5 +1,5 @@
-import { ProductBody } from "./product-body.dto";
-import { SearchModifyProductImageDto } from "./search-modify-product-image.dto";
+import { ProductBody } from "./request/product-body.dto";
+import { SearchModifyProductImageDto } from "./request/search-modify-product-image.dto";
 
 export class SearchModifyProductDto extends SearchModifyProductImageDto {
   body: ProductBody;
