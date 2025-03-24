9453536206d781bc10f55c17b127e72b177c2149
idooru <shere1765@gmail.com>
2025-03-24 09:30:33 +0900

    [생성] insert-review-image.dto.ts

--- src/model/review/dto/request/insert-review-image.dto.ts
+++ src/model/review/dto/request/insert-review-image.dto.ts
@@ -1,5 +1,4 @@
-import { ReviewEntity } from "../entities/review.entity";
-import { ReviewImageEntity } from "../../media/entities/review-image.entity";
+import { ReviewImageEntity } from "../../../media/entities/review-image.entity";
 
 export class InsertReviewImagesDto {
   reviewId: string;
