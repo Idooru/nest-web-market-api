2c261d29626ba847e1eab7286c449b28d5d7e424
idooru <shere1765@gmail.com>
2025-03-24 10:22:34 +0900

    [생성] find-email.dto.ts

--- src/model/user/dto/request/find-email.dto.ts
+++ src/model/user/dto/request/find-email.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty, PickType } from "@nestjs/swagger";
-import { UserProfileEntity } from "../entities/user-profile.entity";
+import { UserProfileEntity } from "../../entities/user-profile.entity";
 
 export class FindEmailDto extends PickType(UserProfileEntity, ["realName", "phoneNumber"] as const) {
   @ApiProperty({
