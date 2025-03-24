78baba35c2c6caa4978dc0cde641b614189dde6f
idooru <shere1765@gmail.com>
2025-03-24 10:21:31 +0900

    [생성] modify-user-phonenumber.dto.ts

--- src/model/user/dto/request/modify-user-phonenumber.dto.ts
+++ src/model/user/dto/request/modify-user-phonenumber.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty, PickType } from "@nestjs/swagger";
-import { UserProfileEntity } from "../entities/user-profile.entity";
+import { UserProfileEntity } from "../../entities/user-profile.entity";
 
-export class ModifyUserPhonenumberDto extends PickType(UserProfileEntity, ["phoneNumber"] as const) {
+export class ModifyUserPhoneNumberDto extends PickType(UserProfileEntity, ["phoneNumber"] as const) {
   @ApiProperty({
     description: "사용자 전화번호",
     example: "010-1234-5678",
