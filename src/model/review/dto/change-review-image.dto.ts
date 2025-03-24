1d3b1be0c6114f488638bc590a82428c44c138a0
idooru <shere1765@gmail.com>
2025-03-24 09:29:00 +0900

    ?

--- src/model/review/dto/change-review-image.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ReviewImageEntity } from "../../media/entities/review-image.entity";
-
-export class ChangeReviewImageDto {
-  public reviewId: string;
-  public beforeReviewImages: ReviewImageEntity[];
-  public newReviewImages: ReviewImageEntity[];
-}
--- src/model/review/dto/change-review-video.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ReviewVideoEntity } from "../../media/entities/review-video.entity";
-
-export class ChangeReviewVideoDto {
-  public beforeReviewVideos: ReviewVideoEntity[];
-  public newReviewVideos: ReviewVideoEntity[];
-  public reviewId: string;
-}
