0fae0463024f4d9081f0e452cf88958f6f2013c5
idooru <shere1765@gmail.com>
2025-03-26 19:35:44 +0900

    [수정] send-mail-to-admin-about-inquiry-request.dto.ts 파일 경로 변경

--- src/model/inquiry/dto/inquiry-request/response/send-mail-to-admin-about-inquiry-request.dto.ts
+++ src/model/inquiry/dto/inquiry-request/response/send-mail-to-admin-about-inquiry-request.dto.ts
@@ -1,6 +1,6 @@
 import { ProductEntity } from "src/model/product/entities/product.entity";
-import { InquiryRequestEntity } from "../../entities/inquiry-request.entity";
-import { ClientUserEntity } from "../../../user/entities/client-user.entity";
+import { InquiryRequestEntity } from "../../../entities/inquiry-request.entity";
+import { ClientUserEntity } from "../../../../user/entities/client-user.entity";
 
 export class SendMailToAdminAboutInquiryRequestDto {
   public product: ProductEntity;
