6b22ac0832d69d7fc0192f329cdec3e9069ed461
idooru <shere1765@gmail.com>
2025-03-26 13:55:36 +0900

    [생성] user-profile-raw.dto.ts

---
+++ src/model/user/dto/response/user-profile-raw.dto.ts
@@ -0,0 +1,11 @@
+export class UserProfileRawDto {
+  userId: string;
+  userRole: string;
+  realName: string;
+  birth: string;
+  gender: string;
+  phoneNumber: string;
+  address: string;
+  nickName: string;
+  email: string;
+}
