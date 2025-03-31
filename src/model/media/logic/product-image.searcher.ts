b1aa59fb0a4d25c20cfc3c76ca68e3756d5bce2b
idooru <shere1765@gmail.com>
2025-03-31 23:18:32 +0900

    [생성] product-image.searcher.ts

---
+++ src/model/media/logic/product-image.searcher.ts
@@ -0,0 +1,25 @@
+import { FindEntityArgs, Searcher } from "../../../common/interfaces/search/searcher";
+import { Injectable } from "@nestjs/common";
+import { Implemented } from "../../../common/decorators/implemented.decoration";
+import { ProductImageSearchRepository } from "../repositories/product-image-search.repository";
+import { ProductImageEntity } from "../entities/product-image.entity";
+import { MediaBasicRawDto } from "../dto/response/media-basic-raw.dto";
+import { MediaCookieDto } from "../dto/request/media-cookie.dto";
+
+@Injectable()
+export class ProductImageSearcher implements Searcher<ProductImageEntity> {
+  constructor(private readonly productImageSearchRepository: ProductImageSearchRepository) {}
+
+  @Implemented
+  public findEntity(args: FindEntityArgs): Promise<ProductImageEntity | ProductImageEntity[]> {
+    const { property, alias, getOne, joinEntities } = args;
+    if (joinEntities && joinEntities.length) {
+      return this.productImageSearchRepository.findOptionalEntity({ property, alias, joinEntities, getOne });
+    }
+    return this.productImageSearchRepository.findPureEntity({ property, alias, getOne });
+  }
+
+  public async findAllRaws(dto: MediaCookieDto[]): Promise<MediaBasicRawDto[]> {
+    return this.productImageSearchRepository.findAllRaws(dto);
+  }
+}
