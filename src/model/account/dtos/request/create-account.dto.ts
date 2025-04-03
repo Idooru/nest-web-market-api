f66c9b08076ae1a93da7c7b534b11d69224c0e85
idooru <shere1765@gmail.com>
2025-04-03 12:42:21 +0900

    [수정] create-account.dto.ts 파일 경로 변경

--- src/model/account/dtos/request/create-account.dto.ts
+++ src/model/account/dtos/request/create-account.dto.ts
@@ -1,5 +1,5 @@
-import { UserEntity } from "../../user/entities/user.entity";
 import { AccountBody } from "./account-body.dto";
+import { UserEntity } from "../../../user/entities/user.entity";
 
 export class CreateAccountDto {
   public user: UserEntity;
