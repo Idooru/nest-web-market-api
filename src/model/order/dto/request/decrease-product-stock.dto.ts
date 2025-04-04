b725cbb52dd526ff35521ad985e9cead7e216102
idooru <shere1765@gmail.com>
2025-04-04 12:35:37 +0900

    [수정] decrease-product-stock.dto.ts 파일 경로 변경

--- src/model/order/dto/request/decrease-product-stock.dto.ts
+++ src/model/order/dto/request/decrease-product-stock.dto.ts
@@ -1,4 +1,4 @@
-import { ProductEntity } from "../../product/entities/product.entity";
+import { ProductEntity } from "../../../product/entities/product.entity";
 
 export class DecreaseProductStockDto {
   product: ProductEntity;
