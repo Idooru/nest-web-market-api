09d26a76a000ac7f5eee24f0014f3afd208c108a
idooru <shere1765@gmail.com>
2025-04-04 12:36:40 +0900

    [수정] search-create-order.dto.ts 파일 경로 변경

--- src/model/order/dto/request/search-create-order.dto.ts
+++ src/model/order/dto/request/search-create-order.dto.ts
@@ -1,7 +1,7 @@
 import { OrderBody } from "./order-body.dto";
-import { ClientUserEntity } from "../../user/entities/client-user.entity";
-import { AccountEntity } from "../../account/entities/account.entity";
-import { ProductQuantity } from "../types/product-quantity.type";
+import { ClientUserEntity } from "../../../user/entities/client-user.entity";
+import { ProductQuantity } from "../../types/product-quantity.type";
+import { AccountEntity } from "../../../account/entities/account.entity";
 
 export class SearchCreateOrderDto {
   clientId: string;
