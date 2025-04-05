a47a62f42506eb6a9169826f5e5c3343d018bc5f
idooru <shere1765@gmail.com>
2025-04-05 15:52:23 +0900

    [추가] BaseEntity 상속

--- src/common/entities/date.entity.ts
+++ src/common/entities/date.entity.ts
@@ -1,6 +1,6 @@
-import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from "typeorm";
 
-export abstract class DateEntity {
+export abstract class DateEntity extends BaseEntity {
   @CreateDateColumn({ type: "timestamp", select: false })
   public createdAt: Date;
 
