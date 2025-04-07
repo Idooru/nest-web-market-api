68675e2141ab17c160c79dfde300afea89ae0cc8
idooru <shere1765@gmail.com>
2025-04-07 09:50:50 +0900

    [추가] 공통 메서드

--- src/common/interfaces/search/search.repository.ts
+++ src/common/interfaces/search/search.repository.ts
@@ -1,8 +1,35 @@
-import { BaseEntity } from "typeorm";
+import { BaseEntity, SelectQueryBuilder } from "typeorm";
+import { loggerFactory } from "../../functions/logger.factory";
+import { InternalServerErrorException } from "@nestjs/common";
 
-export interface SearchRepository<T> {
-  findPureEntity(args: FindPureEntityArgs): Promise<T | T[]>;
-  findOptionalEntity(args: FindOptionalEntityArgs): Promise<T | T[]>;
+export abstract class SearchRepository<E, D, R> {
+  abstract findPureEntity(args: FindPureEntityArgs): Promise<E | E[]>;
+  abstract findOptionalEntity(args: FindOptionalEntityArgs): Promise<E | E[]>;
+  abstract findAllRaws(dto: D | D[]): Promise<R[]>;
+
+  protected joinEntity(entities: (typeof BaseEntity)[], query: SelectQueryBuilder<E>, select: string): void {
+    const targetEntity = `${select.charAt(0).toUpperCase() + select.slice(1)}Entity`;
+    entities.forEach((entity) => {
+      const entityName = entity.name.replace("Entity", "");
+      try {
+        query.leftJoinAndSelect(`${select}.${entityName}`, entityName);
+      } catch (err) {
+        if (err.message.includes("entity was not found")) {
+          const message = `${entityName}Entity는 ${targetEntity}와 연간관계가 없습니다.`;
+          loggerFactory(`None Relation With ${targetEntity}`).error(message);
+          throw new InternalServerErrorException(message);
+        } else {
+          const message = err.message;
+          loggerFactory("Unexpected Exception").error(message);
+          throw new InternalServerErrorException(message);
+        }
+      }
+    });
+  }
+
+  protected getEntity(getOne: boolean, query: SelectQueryBuilder<E>): Promise<E | E[]> {
+    return getOne ? query.getOne() : query.getMany();
+  }
 }
 
 export class FindPureEntityArgs {
@@ -14,6 +41,6 @@ export class FindPureEntityArgs {
 export class FindOptionalEntityArgs {
   property: string;
   alias: unknown;
-  joinEntities: (typeof BaseEntity)[];
+  entities: (typeof BaseEntity)[];
   getOne: boolean;
 }
