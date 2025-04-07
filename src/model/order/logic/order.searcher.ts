4a0abbe0d04a631a378cca5cc30282a4b5884f20
idooru <shere1765@gmail.com>
2025-04-07 10:45:02 +0900

    [수정] searcher 클래스 최적화

--- src/model/order/logic/order.searcher.ts
+++ src/model/order/logic/order.searcher.ts
@@ -1,12 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { OrderSearchRepository } from "../repositories/order-search.repository";
 import { OrderEntity } from "../entities/order.entity";
+import { FindEntityArgs, Searcher } from "../../../common/interfaces/search/searcher";
+import { Implemented } from "../../../common/decorators/implemented.decoration";
+import { FindAllOrdersDto } from "../dto/request/find-all-orders.dto";
+import { OrderBasicRawDto } from "../dto/response/order-basic-raw.dto";
 
 @Injectable()
-export class OrderSearcher {
+export class OrderSearcher implements Searcher<OrderEntity, FindAllOrdersDto, OrderBasicRawDto> {
   constructor(private readonly orderSearchRepository: OrderSearchRepository) {}
 
-  public findOrders(userId: string): Promise<OrderEntity[]> {
-    return this.orderSearchRepository.findOrderRows(userId);
+  @Implemented
+  public findEntity(args: FindEntityArgs): Promise<OrderEntity | OrderEntity[]> {
+    const { property, alias, getOne, entities } = args;
+    if (entities && entities.length) {
+      return this.orderSearchRepository.findOptionalEntity({ property, alias, entities, getOne });
+    }
+    return this.orderSearchRepository.findPureEntity({ property, alias, getOne });
+  }
+
+  @Implemented
+  public findAllRaws(dto: FindAllOrdersDto): Promise<OrderBasicRawDto[]> {
+    return this.orderSearchRepository.findAllRaws(dto);
   }
 }
