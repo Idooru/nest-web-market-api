deee108960c6a43ebc32bf1ef34d5b9689badb7b
idooru <shere1765@gmail.com>
2025-04-07 09:51:50 +0900

    [추가] 전체 값을 가져오는 메서드

--- src/common/interfaces/search/searcher.ts
+++ src/common/interfaces/search/searcher.ts
@@ -1,12 +1,13 @@
 import { BaseEntity } from "typeorm";
 
-export interface Searcher<T> {
+export interface Searcher<T, D, R> {
   findEntity(args: FindEntityArgs): Promise<T | T[]>;
+  findAllRaws(dto: D | D[]): Promise<R[]>;
 }
 
 export class FindEntityArgs {
   property: string;
   alias: unknown;
   getOne: boolean;
-  joinEntities?: (typeof BaseEntity)[];
+  entities?: (typeof BaseEntity)[];
 }
