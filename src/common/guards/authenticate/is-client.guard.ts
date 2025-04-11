b8214da4d610a6f4ad5cefd58512cb46d521a8fb
idooru <shere1765@gmail.com>
2025-04-11 22:27:33 +0900

    [수정] req.user 타입 장착

--- src/common/guards/authenticate/is-client.guard.ts
+++ src/common/guards/authenticate/is-client.guard.ts
@@ -2,13 +2,14 @@ import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@
 import { Request } from "express";
 import { loggerFactory } from "../../functions/logger.factory";
 import { Implemented } from "../../decorators/implemented.decoration";
+import { JwtAccessTokenPayload } from "../../../model/auth/jwt/jwt-access-token-payload.interface";
 
 @Injectable()
 export class IsClientGuard implements CanActivate {
   @Implemented
   public canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest() as Request;
-    const { user } = req;
+    const user = req.user as JwtAccessTokenPayload;
 
     if (user.userRole !== "client") {
       const message = "client 계정만 수행 할 수 있는 작업입니다.";
